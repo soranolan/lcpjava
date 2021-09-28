286970808718f3b0e56ee799e2224495e910099c
Sorano Lan <soranolan@example.com>
2021-09-28 14:28:43 +0800

    Initial commit

---
+++ src/main/java/com/example/lcpjava/LcpjavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.lcpjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcpjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LcpjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lcpjava/LcpjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lcpjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LcpjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
