0b901b5c6a40fdbf29ac7eae554a7f99fbae30b9
Sorano Lan <soranolan@example.com>
2021-10-01 11:11:13 +0800

    inserting

---
+++ src/main/java/com/example/lcpjava/array/Lcp_1089.java
@@ -0,0 +1,75 @@
+package com.example.lcpjava.array;
+
+public class Lcp_1089 {
+	
+	/**
+	 * time  : O(n)
+	 * space : O(1)
+	 * 
+	 * int length <- arr.length
+	 * int zeroCounts <- 0
+	 * 
+	 * for number in array
+	 * 		if number is zero
+	 * 			zeroCounts++
+	 * 		end if
+	 * end for
+	 * 
+	 * int left <- length - 1
+	 * int right <- length + zeroCounts - 1
+	 * 
+	 * while left not equal right
+	 * 		if right is lower than length
+	 * 			right <-> left
+	 * 		end if
+	 * 		right--
+	 * 		if left is zero
+	 * 			if right is lower than length
+	 * 				right <-> left
+	 * 			end if
+	 * 			right--
+	 * 		end if
+	 * 		left--
+	 * end while
+	 * */
+	public void duplicateZeros(int[] arr) {
+		int length = arr.length;
+		int zeroCounts = 0;
+		for (int i = 0; i < length; i++) {
+			if (arr[i] == 0) { zeroCounts++; }
+		}
+		int left = length - 1;
+		int right = length + zeroCounts - 1;
+		
+		while (left != right) {
+			assign(left, right, length, arr);
+			right--;
+			if (arr[left] == 0) {
+				assign(left, right, length, arr);
+				right--;
+			}
+			left--;
+		}
+		
+		for (int num : arr) { System.out.print(num + " "); }
+		System.out.println();
+	}
+	
+	private void assign(int left, int right, int length, int[] arr) {
+		if (right < length) { arr[right] = arr[left]; }
+	}
+	
+	public static void main(String[] args) {
+		Lcp_1089 lcp = new Lcp_1089();
+		
+		int[] input = { 1, 0, 2, 3, 0, 4, 5, 0 };
+		lcp.duplicateZeros(input);
+		
+		input = new int[] { 1, 2, 3 };
+		lcp.duplicateZeros(input);
+		
+		input = new int[] { 0, 0, 0, 0, 0, 0, 0 };
+		lcp.duplicateZeros(input);
+	}
+	
+}
---
+++ src/main/java/com/example/lcpjava/array/Lcp_88.java
@@ -0,0 +1,84 @@
+package com.example.lcpjava.array;
+
+public class Lcp_88 {
+	
+	/**
+	 * time  : O(n)
+	 * space : O(1)
+	 * 
+	 * int n1Right <- m - 1
+	 * int n2Right <- n - 1
+	 * int mergeRight <- m + n - 1
+	 * 
+	 * while n1Right grater than and equal to zero && n2Right greater than and equal to zero
+	 * 		if nums1[n1Right] lower than nums2[n2Right]
+	 * 			nums1[mergeRight] <- nums2[n2Right]
+	 * 			n2Right--
+	 * 		else
+	 * 			nums1[mergeRight] <- nums1[n1Right]
+	 * 			n1Right--
+	 * 		end if
+	 * 		mergeRight--
+	 * end while
+	 * 
+	 * handle nums1 is empty
+	 * while n2Right greater than and equal to zero
+	 * 		nums1[mergeRight] <- nums2[n2Right]
+	 * end while
+	 * */
+	public void merge(int[] nums1, int m, int[] nums2, int n) {
+		int n1Right = m - 1;
+		int n2Right = n - 1;
+		int mergeRight = m + n - 1;
+		
+		while (n1Right >= 0 && n2Right >= 0) {
+			if (nums1[n1Right] < nums2[n2Right]) {
+				assign(n2Right, mergeRight, nums1, nums2);
+				n2Right--;
+			} else {
+				nums1[mergeRight] = nums1[n1Right];
+				n1Right--;
+			}
+			mergeRight--;
+		}
+		
+		while (n2Right >= 0) {
+			assign(n2Right, mergeRight, nums1, nums2);
+			n2Right--;
+			mergeRight--;
+		}
+		
+		for (int num : nums1) { System.out.print(num + " "); }
+		System.out.println();
+	}
+	
+	private void assign(int n2Right, int mergeRight, int[] nums1, int[] nums2) {
+		nums1[mergeRight] = nums2[n2Right];
+	}
+	
+	public static void main(String[] args) {
+		Lcp_88 lcp = new Lcp_88();
+		
+		// 1 2 2 3 5 6
+		int[] nums1 = { 1, 2, 3, 0, 0, 0 };
+		int m = 3;
+		int[] nums2 = { 2, 5, 6 };
+		int n = 3;
+		lcp.merge(nums1, m, nums2, n);
+		
+		// 1
+		nums1 = new int[] { 1 };
+		m = 1;
+		nums2 = new int[] {};
+		n = 0;
+		lcp.merge(nums1, m, nums2, n);
+		
+		// 1
+		nums1 = new int[] { 0 };
+		m = 0;
+		nums2 = new int[] { 1 };
+		n = 1;
+		lcp.merge(nums1, m, nums2, n);
+	}
+	
+}
