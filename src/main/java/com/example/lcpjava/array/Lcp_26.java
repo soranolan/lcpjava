903d5d063ea38e928265ba4ae3f821dab6007cbb
Sorano Lan <soranolan@example.com>
2021-10-03 14:28:21 +0800

    deleting

---
+++ src/main/java/com/example/lcpjava/array/Lcp_26.java
@@ -0,0 +1,89 @@
+package com.example.lcpjava.array;
+
+public class Lcp_26 {
+	
+	/**
+	 * time  : O(n)
+	 * space : O(1)
+	 * 
+	 * int left = 0
+	 * int right = 1
+	 * 
+	 * while right is lower than array length
+	 * 		if array[left] is equal to array[right]
+	 * 			right++
+	 * 		else
+	 * 			left++
+	 * 			array[left] = array[right]
+	 * 			right++
+	 * 		end if
+	 * end while
+	 * 
+	 * return left + 1
+	 * */
+	public int removeDuplicates_1(int[] nums) {
+		int left = 0;
+		int right = 1;
+		
+		while (right < nums.length) {
+			if (nums[left] == nums[right]) {
+				right++;
+			} else {
+				left++;
+				nums[left] = nums[right];
+				right++;
+			}
+		}
+		
+		for (int num : nums) { System.out.print(num + " "); }
+		System.out.println();
+		return left + 1;
+	}
+	
+	/**
+	 * time  : O(n)
+	 * space : O(1)
+	 * 
+	 * int left <- 0
+	 * 
+	 * for int right <- 1 to end
+	 * 		if array[left] not equal to array[right]
+	 * 			left++
+	 * 			array[left] = array[right]
+	 * 		end if
+	 * end for
+	 * 
+	 * return left + 1
+	 * */
+	public int removeDuplicates(int[] nums) {
+		int left = 0;
+		
+		for (int right = 1; right < nums.length; right++) {
+			if (nums[left] != nums[right]) {
+				left++;
+				nums[left] = nums[right];
+			}
+		}
+		
+		for (int num : nums) { System.out.print(num + " "); }
+		System.out.println();
+		return left + 1;
+	}
+	
+	public static void main(String[] args) {
+		Lcp_26 lcp = new Lcp_26();
+		int[] input = null;
+		int output = -1;
+		
+		// 2; 1 2
+		input = new int[] { 1, 1, 2 };
+		output = lcp.removeDuplicates(input);
+		System.out.println(output);
+		
+		// 5; 0 1 2 3 4
+		input = new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+		output = lcp.removeDuplicates(input);
+		System.out.println(output);
+	}
+	
+}
---
+++ src/main/java/com/example/lcpjava/array/Lcp_27.java
@@ -0,0 +1,115 @@
+package com.example.lcpjava.array;
+
+public class Lcp_27 {
+	
+	/**
+	 * time  : O(n)
+	 * space : O(1)
+	 * 
+	 * int count <- 0
+	 * 
+	 * for number in array
+	 * 		if number is equal to val
+	 * 			count++
+	 * 		end if
+	 * end for
+	 * 
+	 * int length <- array.length
+	 * int left <- 0
+	 * int right <- length - 1
+	 * 
+	 * while left is not equal right
+	 * 		if array[left] is equal to val
+	 * 			array[left] = array[right]
+	 * 			array[right] = val
+	 * 			right--
+	 * 		else
+	 * 			left++
+	 * 		end if
+	 * end while
+	 * 
+	 * return length - count
+	 * */
+	public int removeElement_1(int[] nums, int val) {
+		int count = 0;
+		for (int num : nums) {
+			if (num == val) { count++; }
+		}
+		
+		int length = nums.length;
+		int left = 0;
+		int right = length - 1;
+		
+		while (left < right) {
+			if (nums[left] == val) {
+				nums[left] = nums[right];
+				nums[right] = val;
+				right--;
+			} else {
+				left++;
+			}
+		}
+		
+		for (int num : nums) { System.out.print(num + " "); }
+		System.out.println();
+		
+		return length - count;
+	}
+	
+	/**
+	 * time  : O(n)
+	 * space : O(1)
+	 * 
+	 * int count <- 0
+	 * 
+	 * for int i <- 0 to array end
+	 * 		if array[i] is not equal to val
+	 * 			array[count] = array[i]
+	 * 			count++
+	 * 		end if
+	 * end for
+	 * 
+	 * return count
+	 * */
+	public int removeElement(int[] nums, int val) {
+		int count = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] != val) {
+				nums[count] = nums[i];
+				count++;
+			}
+		}
+		return count;
+	}
+	
+	public static void main(String[] args) {
+		Lcp_27 lcp = new Lcp_27();
+		int output = -1;
+		int val = -1;
+		
+		// 2; 2 2
+		int[] input = { 3, 2, 2, 3 };
+		val = 3;
+		output = lcp.removeElement(input, val);
+		System.out.println(output);
+		
+		// 5; 0 1 2 3 4
+		input = new int[] { 0, 1, 2, 2, 3, 0, 4, 2 };
+		val = 2;
+		output = lcp.removeElement(input, val);
+		System.out.println(output);
+		
+		// 0;
+		input = new int[] { };
+		val = 0;
+		output = lcp.removeElement(input, val);
+		System.out.println(output);
+		
+		// 0;
+		input = new int[] { 1 };
+		val = 1;
+		output = lcp.removeElement(input, val);
+		System.out.println(output);
+	}
+	
+}
