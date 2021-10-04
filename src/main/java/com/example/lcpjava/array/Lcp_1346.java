354b6c04ed5fc2f2976d76abaf9a4d7f1db440a1
Sorano Lan <soranolan@example.com>
2021-10-04 18:19:18 +0800

    searching

---
+++ src/main/java/com/example/lcpjava/array/Lcp_1346.java
@@ -0,0 +1,116 @@
+package com.example.lcpjava.array;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/** Check If N and Its Double Exist */
+public class Lcp_1346 {
+	
+	/**
+	 * time  : O(n)
+	 * space : O(1)
+	 * 
+	 * int left <- 0
+	 * int right <- 1
+	 * int length <- array length
+	 * 
+	 * if left is equal to zero or is equal to one
+	 * 		return false
+	 * end if
+	 * 
+	 * while left lower than length
+	 * 		if array[left] is equal to array[right] multiple two || array[right] is equal to array[left] multiple two
+	 * 			return true
+	 * 		else
+	 * 			right++
+	 * 		end if
+	 * 		if left plus two is equal to length
+	 * 			return false
+	 * 		end if
+	 * 		if right is equal to length
+	 * 			left++
+	 * 			right <- left + 1
+	 * 		end if
+	 * end while
+	 * 
+	 * return false
+	 * */
+	public boolean checkIfExist_1(int[] arr) {
+		int left = 0;
+		int right = 1;
+		int length = arr.length;
+		
+		if (length == 0 || length == 1) { return false; }
+		
+		while (left < length) {
+			if (arr[left] == arr[right] * 2 || arr[right] == arr[left] * 2) {
+				return true;
+			} else {
+				right++;
+			}
+			if ((left + 2) == length) { return false; }
+			if (right == length) {
+				left++;
+				right = left + 1;
+			}
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * time  : O(n)
+	 * space : O(n)
+	 * 
+	 * Set<Integer> container
+	 * 
+	 * for int i <- 0 to array end
+	 * 		if container contains arr[i] multiple two || arr[i] mod two is equal to zero and container contains arr[i] divide two
+	 * 			return true
+	 * 		end if
+	 * 		container add arr[i]
+	 * end for
+	 * 
+	 * return false
+	 * */
+	public boolean checkIfExist(int[] arr) {
+		Set<Integer> container = new HashSet<>();
+		for (int i = 0; i < arr.length; i++) {
+			if (container.contains(arr[i] * 2) || (arr[i] % 2 == 0 && container.contains(arr[i] / 2))) { return true; }
+			container.add(arr[i]);
+		}
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		Lcp_1346 lcp = new Lcp_1346();
+		int[] input = null;
+		boolean output = false;
+		
+		// true
+		input = new int[] { 10, 2, 5, 3 };
+		output = lcp.checkIfExist(input);
+		System.out.println(output);
+		
+		// true
+		input = new int[] { 7, 1, 14, 11 };
+		output = lcp.checkIfExist(input);
+		System.out.println(output);
+		
+		// false
+		input = new int[] { 3, 1, 7, 11 };
+		output = lcp.checkIfExist(input);
+		System.out.println(output);
+		
+		// false
+		input = new int[] { };
+		output = lcp.checkIfExist(input);
+		System.out.println(output);
+		
+		// false
+		input = new int[] { 1 };
+		output = lcp.checkIfExist(input);
+		System.out.println(output);
+	}
+	
+}
---
+++ src/main/java/com/example/lcpjava/array/Lcp_941.java
@@ -0,0 +1,161 @@
+package com.example.lcpjava.array;
+
+/** Valid Mountain Array */
+public class Lcp_941 {
+	
+	/**
+	 * time    : O(n)
+	 * space   : O(1)
+	 * 
+	 * boolean increasing <- false
+	 * boolean decreasing <- false
+	 * int length <- array length
+	 * 
+	 * if array is null || length is lower than three
+	 * 		return fasle
+	 * end if
+	 * 
+	 * for int left <- 0 to length - 1
+	 * 		if array[left] is lower than array[left + 1]
+	 * 			if decreasing is true
+	 * 				return false
+	 * 			end if
+	 * 			increasing <- true
+	 * 		else if array[left] is greater than array[left + 1]
+	 * 			if increasing is false
+	 * 				return false
+	 * 			end if
+	 * 			decreasing <- true
+	 * 		else
+	 * 			return false
+	 * 		end if
+	 * end for
+	 * 
+	 * return increasing && decreasing
+	 * */
+	public boolean validMountainArray_1(int[] arr) {
+		boolean increasing = false;
+		boolean decreasing = false;
+		int length = arr.length;
+		if (arr == null || length < 3) { return false; }
+		
+		for (int left = 0; left < length - 1; left++) {
+			if (arr[left] < arr[left + 1]) {
+				if (decreasing) { return false; }
+				increasing = true;
+			} else if (arr[left] > arr[left + 1]) {
+				if (!increasing) { return false; }
+				decreasing = true;
+			} else {
+				return false;
+			}
+		}
+		
+		return increasing && decreasing;
+	}
+	
+	/**
+	 * time    : O(n)
+	 * space   : O(1)
+	 * 
+	 * int length <- array length
+	 * int i <- 0
+	 * 
+	 * while i + 1 is lower than length && array[i] is lower than array[i + 1]
+	 * 		i++
+	 * end while
+	 * 
+	 * if i is equal to zero || i is equal to length - 1
+	 * 		return false
+	 * end if
+	 * 
+	 * while i + 1 is lower than length && array[i] is greater than array[i + 1]
+	 * 		i++
+	 * end while
+	 * 
+	 * return i == length - 1
+	 * */
+	public boolean validMountainArray_2(int[] arr) {
+		int length = arr.length;
+		int i = 0;
+		
+		while (i + 1 < length && arr[i] < arr[i + 1]) { i++; }
+		
+		if (i == 0 || i == length - 1) { return false; }
+		
+		while (i + 1 < length && arr[i] > arr[i + 1]) { i++; }
+		
+		return i == length - 1;
+	}
+	
+	/**
+	 * time    : O(n)
+	 * space   : O(1)
+	 * 
+	 * int length <- array length
+	 * if array is null || length is lower than three || array[0] is greater than or equal to array[1]
+	 * 		return false
+	 * end if
+	 * 
+	 * boolean increasing <- true
+	 * 
+	 * for int left <- 2 to array end
+	 * 		if increasing is true
+	 * 			if array[left - 1] is equal to array[left]
+	 * 				return false
+	 * 			else if array[left - 1] is greater than array[left]
+	 * 				increasing <- false
+	 * 		else
+	 * 			if array[left - 1] <= array[left]
+	 * 				return false
+	 * 			end if
+	 * 		end if
+	 * end for
+	 * 
+	 * return !increasing
+	 * */
+	public boolean validMountainArray(int[] arr) {
+		int length = arr.length;
+		if (arr == null || length < 3 || arr[0] >= arr[1]) { return false; }
+		
+		boolean increasing = true;
+		for (int left = 2; left < arr.length; left++) {
+			if (increasing) {
+				if (arr[left - 1] == arr[left]) {
+					return false;
+				} else if (arr[left - 1] > arr[left]) {
+					increasing = false;
+				}
+			} else {
+				if (arr[left - 1] <= arr[left]) {
+					return false;
+				}
+			}
+		}
+		
+		return !increasing;
+	}
+	
+	public static void main(String[] args) {
+		Lcp_941 lcp = new Lcp_941();
+		int[] input = null;
+		boolean output = false;
+		
+		input = new int[] { 2, 1 };
+		output = lcp.validMountainArray(input);
+		System.out.println(output);
+		
+		input = new int[] { 3, 5, 5 };
+		output = lcp.validMountainArray(input);
+		System.out.println(output);
+		
+		input = new int[] { 0, 3, 2, 1 };
+		output = lcp.validMountainArray(input);
+		System.out.println(output);
+		
+		input = new int[] { 2, 0, 2 };
+		output = lcp.validMountainArray(input);
+		System.out.println(output);
+	}
+	
+}
