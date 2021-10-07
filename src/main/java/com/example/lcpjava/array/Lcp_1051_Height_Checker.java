2329d6c7fcc282b3643373e4713cf686b9d00a6c
Sorano Lan <soranolan@example.com>
2021-10-07 18:04:28 +0800

    conclusion

---
+++ src/main/java/com/example/lcpjava/array/Lcp_1051_Height_Checker.java
@@ -0,0 +1,108 @@
+package com.example.lcpjava.array;
+
+import java.util.Arrays;
+
+public class Lcp_1051_Height_Checker {
+	
+	/**
+	 * time  : O(n log n)
+	 * space : O(n)
+	 * 
+	 * int count <- 0
+	 * int[] clone <- array clone
+	 * array sort clone
+	 * 
+	 * for int i <- 0 to array end
+	 * 		if array[i] is not equal to clone[i]
+	 * 			count++
+	 * 		end if
+	 * end for
+	 * 
+	 * return count
+	 * */
+	public int heightChecker_1(int[] heights) {
+		int count = 0;
+		int[] clone = heights.clone();
+		Arrays.sort(clone);
+		
+		for (int i = 0; i < heights.length; i++) {
+			if (heights[i] != clone[i]) { count++; }
+		}
+		
+		return count;
+	}
+	
+	/**
+	 * time  : O(max(max(height), length) ?
+	 *         O(max(height)) while loop
+	 *         O(n)           for loop
+	 * space : O(n)
+	 * 
+	 * count sort
+	 * 
+	 * int[] bucket <- size constraint + 1
+	 * 
+	 * for number in array
+	 * 		bucket[number]++
+	 * end for
+	 * 
+	 * int count <- 0
+	 * int bucketIndex <- 0
+	 * 
+	 * for int i <- 0 to array end
+	 * 		while bucket[bucketIndex] == 0
+	 * 			bucketIndex++
+	 * 		end while
+	 * 		
+	 * 		if array[i] is not equal to bucketIndex
+	 * 			count++
+	 * 		end if
+	 * 		
+	 * 		bucket[bucketIndex]--
+	 * end for
+	 * 
+	 * return count
+	 * */
+	public int heightChecker(int[] heights) {
+		int[] bucket = new int[101];
+		for (int height : heights) { bucket[height]++; }
+		
+		int count = 0;
+		int bucketIndex = 0;
+		
+		for (int i = 0; i < heights.length; i++) {
+			while (bucket[bucketIndex] == 0) { bucketIndex++; }
+			if (heights[i] != bucketIndex) { count++; }
+			bucket[bucketIndex]--;
+		}
+		
+		return count;
+	}
+	
+	public static void main(String[] args) {
+		Lcp_1051_Height_Checker lcp = new Lcp_1051_Height_Checker();
+		int[] input = null;
+		int output = -1;
+		
+		// 3
+		input = new int[] { 1, 1, 4, 2, 1, 3 };
+		output = lcp.heightChecker(input);
+		System.out.println(output);
+		
+		// 5
+		input = new int[] { 5, 1, 2, 3, 4 };
+		output = lcp.heightChecker(input);
+		System.out.println(output);
+		
+		// 0
+		input = new int[] { 1, 2, 3, 4, 5 };
+		output = lcp.heightChecker(input);
+		System.out.println(output);
+		
+		// 16
+		input = new int[] { 0, 100, 0, 99, 0, 98, 0, 97, 0, 96, 0, 95, 0, 94, 0, 93, 0, 92, 0, 91, 0, 90 };
+		output = lcp.heightChecker(input);
+		System.out.println(output);
+	}
+	
+}
---
+++ src/main/java/com/example/lcpjava/array/Lcp_414_Third_Maximum_Number.java
@@ -0,0 +1,201 @@
+package com.example.lcpjava.array;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Set;
+
+public class Lcp_414_Third_Maximum_Number {
+	
+	/**
+	 * time  : O(n)
+	 * space : O(1)
+	 * 
+	 * Integer max1 <- null
+	 * Integer max2 <- null
+	 * Integer max3 <- null
+	 * 
+	 * for number in array
+	 * 		if number is equal to max1 or max2 or max3
+	 * 			continue
+	 * 		end if
+	 * 		if max1 is equal to null || number is greater than max1
+	 * 			max3 <- max2
+	 * 			max2 <- max1
+	 * 			max1 <- number
+	 * 		else if max2 is equal to null || number is greater than max2
+	 * 			max3 <- max2
+	 * 			max2 <- number
+	 * 		else if max3 is equal to null || number is greater than max3
+	 * 			max3 <- number
+	 * 		end if
+	 * end for
+	 * 
+	 * return max3 == null ? max1 : max3
+	 * */
+	public int thirdMax_1(int[] nums) {
+		Integer max1 = null;
+		Integer max2 = null;
+		Integer max3 = null;
+		
+		for (Integer num : nums) {
+			if (num.equals(max1) || num.equals(max2) || num.equals(max3)) { continue; }
+			if (max1 == null || num > max1) {
+				max3 = max2;
+				max2 = max1;
+				max1 = num;
+			} else if (max2 == null || num > max2) {
+				max3 = max2;
+				max2 = num;
+			} else if (max3 == null || num > max3) {
+				max3 = num;
+			}
+		}
+		
+		return max3 == null ? max1 : max3;
+	}
+	
+	/**
+	 * time  : O(n)
+	 * space : O(n)
+	 * 
+	 * PriorityQueue<Integer> pq
+	 * Set<Integer> set
+	 * 
+	 * for number in array
+	 * 		if set does not contain number
+	 * 			set add number
+	 * 			pq offer number
+	 * 			if pq size is greater than three
+	 * 				pq remove
+	 * 			end if
+	 * 		end if
+	 * end for
+	 * 
+	 * if pq size is equal to two
+	 * 		pq remove
+	 * end if
+	 * 
+	 * return pq peek
+	 * */
+	public int thirdMax_2(int[] nums) {
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		Set<Integer> set = new HashSet<>();
+		
+		for (int num : nums) {
+			if (!set.contains(num)) {
+				set.add(num);
+				pq.offer(num);
+				if (pq.size() > 3) { pq.remove(); }
+			}
+		}
+		
+		if (pq.size() == 2) { pq.remove(); }
+		
+		return pq.peek();
+	}
+	
+	/**
+	 * time  : O(n)
+	 * space : O(1)
+	 * 
+	 * PriorityQueue<Integer> pq
+	 * 
+	 * for number in array
+	 * 		if pq does not contain number
+	 * 			pq offer number
+	 * 			if pq size is greater than three
+	 * 				pq poll
+	 * 			end if
+	 * 		end if
+	 * end for
+	 * 
+	 * if pq size is equal to two
+	 * 		pq poll
+	 * end if
+	 * 
+	 * return pq peek
+	 * */
+	public int thirdMax_3(int[] nums) {
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		
+		for (int num : nums) {
+			if (!pq.contains(num)) {
+				pq.offer(num);
+				if (pq.size() > 3) { pq.poll(); }
+			}
+		}
+		
+		if (pq.size() == 2) { pq.poll(); }
+		
+		return pq.peek();
+	}
+	
+	/**
+	 * time  : O(n log n)
+	 * space : O(1)
+	 * 
+	 * array sort
+	 * 
+	 * int length <- array length
+	 * int count = 0;
+	 * int max3 <- array[length - 1]
+	 * 
+	 * for int i <- length - 1 to array head
+	 * 		if array[i] is not equal to array[i - 1]
+	 * 			max3 <- array[i - 1]
+	 * 			count++
+	 * 		end if
+	 * 		if count is equal to three
+	 * 			break
+	 * 		end if
+	 * end for
+	 * 
+	 * if count is equal to two
+	 * 		return array[length - 1]
+	 * end if
+	 * 
+	 * return max3
+	 * */
+	public int thirdMax(int[] nums) {
+		Arrays.sort(nums);
+		
+		int length = nums.length;
+		int max3 = nums[length - 1];
+		int count = 1;
+		
+		for (int i = length - 1; i > 0 ; i--) {
+			if (nums[i] != nums[i - 1]) {
+				max3 = nums[i - 1];
+				count++;
+			}
+			if (count == 3) { break; }
+		}
+		
+		if (count == 2) { return nums[length - 1]; }
+		
+		return max3;
+	}
+	
+	public static void main(String[] args) {
+		Lcp_414_Third_Maximum_Number lcp = new Lcp_414_Third_Maximum_Number();
+		int[] input = null;
+		int output = -1;
+		
+		// 1
+		input = new int[] { 3, 2, 1 };
+		output = lcp.thirdMax(input);
+		System.out.println(output);
+		
+		// 2
+		input = new int[] { 1, 2 };
+		output = lcp.thirdMax(input);
+		System.out.println(output);
+		
+		// 1
+		input = new int[] { 2, 2, 3, 1 };
+		output = lcp.thirdMax(input);
+		System.out.println(output);
+	}
+	
+}
