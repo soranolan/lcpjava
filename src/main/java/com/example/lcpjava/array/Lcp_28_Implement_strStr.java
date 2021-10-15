0aaa01c95e429af72141c2d9c580cd25a0177800
Sorano Lan <soranolan@example.com>
2021-10-15 10:52:53 +0800

    add comment

--- src/main/java/com/example/lcpjava/array/Lcp_28_Implement_strStr.java
+++ src/main/java/com/example/lcpjava/array/Lcp_28_Implement_strStr.java
@@ -55,13 +55,36 @@ public class Lcp_28_Implement_strStr {
 	/**
 	 * time  : O(m + n)
 	 * space : O(n)
+	 * 
+	 * int hIdx <- 0
+	 * int nIdx <- 0
+	 * int hLen <- haystack length
+	 * int nLen <- needle length
+	 * int[] lps <- buildLPS(needle)
+	 * 
+	 * while hIdx is lower than hLen && nIdx is lower than nLen
+	 * 		if haystack charAt(hIdx) is equal to needle charAt(nIdx)
+	 * 			hIdx++
+	 * 			nIdx++
+	 * 		else if nIdx is greater than zero
+	 * 			nIdx <- lps[nIdx - 1]
+	 * 		else
+	 * 			hIdx++
+	 * 		end if
+	 * end while
+	 * 
+	 * if nIdx is equal to nLen
+	 * 		return hIdx minus nIdx
+	 * else
+	 * 		return -1
+	 * end if
 	 * */
 	public int strStr(String haystack, String needle) {
 		int hIdx = 0;
 		int nIdx = 0;
 		int hLen = haystack.length();
 		int nLen = needle.length();
-		int[] lps = buildLPS(needle);
+		int[] lps = buildLPS(needle);																				// T : O(n); S : O(n)
 		
 		while (hIdx < hLen && nIdx < nLen) {																		// T : O(n)
 			if (haystack.charAt(hIdx) == needle.charAt(nIdx)) {
@@ -77,11 +100,35 @@ public class Lcp_28_Implement_strStr {
 		return nIdx == nLen ? hIdx - nIdx : -1;
 	}
 	
+	/**
+	 * time  : O(n)
+	 * space : O(n)
+	 * 
+	 * int left <- 0
+	 * int right <- 1
+	 * int length <- pattern length
+	 * int[] lps <- empty array
+	 * 
+	 * while right is lower than length
+	 * 		if pattern charAt(left) is equal to pattern charAt(right)
+	 * 			lps[right] <- left + 1
+	 * 			left++
+	 * 			right++
+	 * 		else if left is greater than zero
+	 * 			left <- lps[left - 1]
+	 * 		else
+	 * 			lps[right] <- 0
+	 * 			right++
+	 * 		end if
+	 * end while
+	 * 
+	 * return lps
+	 * */
 	public int[] buildLPS(String pattern) {
 		int left = 0;
 		int right = 1;
 		int length = pattern.length();
-		int[] lps = new int[length];
+		int[] lps = new int[length];																				// S : O(n)
 		
 		while (right < length) {																					// T : O(n)
 			if (pattern.charAt(left) == pattern.charAt(right)) {
