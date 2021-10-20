85a2d524800bf9ed957dc38a7a03712f8584bf14
Sorano Lan <soranolan@example.com>
2021-10-20 11:23:57 +0800

    conclusion

---
+++ src/main/java/com/example/lcpjava/array/Lcp_119_Pascals_Triangle_II.java
@@ -0,0 +1,91 @@
+package com.example.lcpjava.array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lcp_119_Pascals_Triangle_II {
+	
+	/**
+	 * time  : O(n ^ 2)
+	 * space : O(n ^ 2)
+	 * 
+	 * List<List<Integer>> outer <- empty list
+	 * List<Integer> inner <- empty list
+	 * 
+	 * for int i <- 0; if i is lower than or equal to index; i--
+	 * 		for int j <- inner size minus one; if j is greater than zero; j--
+	 * 			inner set(j, inner get(j) plus inner get(j minus one))
+	 * 		end for
+	 * 		inner add one
+	 * 		List<Integer> clone <- clone inner
+	 * 		outer add clone
+	 * end for
+	 * 
+	 * return outer get index
+	 * */
+	public List<Integer> getRow_1(int rowIndex) {
+		List<List<Integer>> outer = new ArrayList<>();
+		List<Integer> inner  = new ArrayList<>();
+		
+		for (int i = 0; i <= rowIndex; i++) {																		// T : O(n)
+			for (int j = inner.size() - 1; j > 0; j--) {															// T : O(m)
+				inner.set(j, inner.get(j) + inner.get(j - 1));														// T : O(1)
+			}
+			inner.add(1);																							// T : amortized O(1); S : O(n)
+			List<Integer> clone = new ArrayList<>(inner);															// S : O(n)
+			outer.add(clone);																						// T : amortized O(1); S : O(n)
+		}
+		
+		return outer.get(rowIndex);
+	}
+	
+	/**
+	 * time  : O(n ^ 2)
+	 * space : O(n)
+	 * 
+	 * List<Integer> result <- empty list
+	 * 
+	 * for int i <- 0; if i is lower than or equal to index; i++
+	 * 		for int j <- result size minus one; if j is greater than zero; j--
+	 * 			result set(j, result get(j) plus result get(j minus one))
+	 * 		end for
+	 * 		result add one
+	 * end for
+	 * 
+	 * return result
+	 * */
+	public List<Integer> getRow(int rowIndex) {
+		List<Integer> result = new ArrayList<>();
+		
+		for (int i = 0; i <= rowIndex; i++) {																		// T : O(n)
+			for (int j = result.size() - 1; j > 0; j--) {															// T : O(m)
+				result.set(j, result.get(j) + result.get(j - 1));													// T : O(1)
+			}
+			result.add(1);																							// T : amortized O(1); S : O(n)
+		}
+		
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		Lcp_119_Pascals_Triangle_II lcp = new Lcp_119_Pascals_Triangle_II();
+		int input = -1;
+		List<Integer> output = null;
+		
+		// 1 3 3 1
+		input = 3;
+		output = lcp.getRow(input);
+		System.out.println(output);
+		
+		// 1
+		input = 0;
+		output = lcp.getRow(input);
+		System.out.println(output);
+		
+		// 1 1
+		input = 1;
+		output = lcp.getRow(input);
+		System.out.println(output);
+	}
+	
+}
