621fce9d81295288fdf7f0332b5c2bcc78dbaf5e
Sorano Lan <soranolan@example.com>
2021-10-22 16:22:59 +0800

    style

--- src/test/java/com/example/lcpjava/array/Lcp_1051_Height_CheckerTest.java
+++ src/test/java/com/example/lcpjava/array/Lcp_1051_Height_CheckerTest.java
@@ -11,7 +11,7 @@ class Lcp_1051_Height_CheckerTest {
 		Lcp_1051_Height_Checker lcp = new Lcp_1051_Height_Checker();
 		
 		int expect = 3;
-		int[] input = new int[] { 1, 1, 4, 2, 1, 3 };
+		int[] input = { 1, 1, 4, 2, 1, 3 };
 		int test = lcp.heightChecker(input);
 		
 		assertThat(test).isEqualTo(expect);
@@ -22,7 +22,7 @@ class Lcp_1051_Height_CheckerTest {
 		Lcp_1051_Height_Checker lcp = new Lcp_1051_Height_Checker();
 		
 		int expect = 5;
-		int[] input = new int[] { 5, 1, 2, 3, 4 };
+		int[] input = { 5, 1, 2, 3, 4 };
 		int test = lcp.heightChecker(input);
 		
 		assertThat(test).isEqualTo(expect);
@@ -33,7 +33,7 @@ class Lcp_1051_Height_CheckerTest {
 		Lcp_1051_Height_Checker lcp = new Lcp_1051_Height_Checker();
 		
 		int expect = 0;
-		int[] input = new int[] { 1, 2, 3, 4, 5 };
+		int[] input = { 1, 2, 3, 4, 5 };
 		int test = lcp.heightChecker(input);
 		
 		assertThat(test).isEqualTo(expect);
@@ -44,7 +44,7 @@ class Lcp_1051_Height_CheckerTest {
 		Lcp_1051_Height_Checker lcp = new Lcp_1051_Height_Checker();
 		
 		int expect = 16;
-		int[] input = new int[] { 0, 100, 0, 99, 0, 98, 0, 97, 0, 96, 0, 95, 0, 94, 0, 93, 0, 92, 0, 91, 0, 90 };
+		int[] input = { 0, 100, 0, 99, 0, 98, 0, 97, 0, 96, 0, 95, 0, 94, 0, 93, 0, 92, 0, 91, 0, 90 };
 		int test = lcp.heightChecker(input);
 		
 		assertThat(test).isEqualTo(expect);
--- src/test/java/com/example/lcpjava/array/Lcp_724_Find_Pivot_IndexTest.java
+++ src/test/java/com/example/lcpjava/array/Lcp_724_Find_Pivot_IndexTest.java
@@ -11,7 +11,7 @@ class Lcp_724_Find_Pivot_IndexTest {
 		Lcp_724_Find_Pivot_Index lcp = new Lcp_724_Find_Pivot_Index();
 		
 		int expect = 3;
-		int[] input = new int[] { 1, 7, 3, 6, 5, 6 };
+		int[] input = { 1, 7, 3, 6, 5, 6 };
 		int test = lcp.pivotIndex(input);
 		
 		assertThat(test).isEqualTo(expect);
@@ -22,7 +22,7 @@ class Lcp_724_Find_Pivot_IndexTest {
 		Lcp_724_Find_Pivot_Index lcp = new Lcp_724_Find_Pivot_Index();
 		
 		int expect = -1;
-		int[] input = new int[] { 1, 2, 3 };
+		int[] input = { 1, 2, 3 };
 		int test = lcp.pivotIndex(input);
 		
 		assertThat(test).isEqualTo(expect);
@@ -33,7 +33,7 @@ class Lcp_724_Find_Pivot_IndexTest {
 		Lcp_724_Find_Pivot_Index lcp = new Lcp_724_Find_Pivot_Index();
 		
 		int expect = 0;
-		int[] input = new int[] { 2, 1, -1 };
+		int[] input = { 2, 1, -1 };
 		int test = lcp.pivotIndex(input);
 		
 		assertThat(test).isEqualTo(expect);
@@ -44,7 +44,7 @@ class Lcp_724_Find_Pivot_IndexTest {
 		Lcp_724_Find_Pivot_Index lcp = new Lcp_724_Find_Pivot_Index();
 		
 		int expect = -1;
-		int[] input = new int[] { -1, -1, -1, -1, -1, -1 };
+		int[] input = { -1, -1, -1, -1, -1, -1 };
 		int test = lcp.pivotIndex(input);
 		
 		assertThat(test).isEqualTo(expect);
