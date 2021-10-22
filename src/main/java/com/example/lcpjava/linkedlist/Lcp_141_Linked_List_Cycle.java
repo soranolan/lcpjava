ec6d20824997dd258fc02cef7189acd08c6ef313
Sorano Lan <soranolan@example.com>
2021-10-22 12:56:02 +0800

    two pointer technique

---
+++ src/main/java/com/example/lcpjava/linkedlist/Lcp_141_Linked_List_Cycle.java
@@ -0,0 +1,44 @@
+package com.example.lcpjava.linkedlist;
+
+public class Lcp_141_Linked_List_Cycle {
+	
+	class ListNode {
+		int val;
+		ListNode next;
+		ListNode(int x) {
+			val = x;
+			next = null;
+		}
+	}
+	
+	/**
+	 * time  : O(n)
+	 * space : O(1)
+	 * 
+	 * ListNode slow <- head
+	 * ListNode fast <- head
+	 * 
+	 * while fast is not null && fast next is not null
+	 * 		slow <- slow next
+	 * 		fast <- fast next next
+	 * 		if slow is equal to fast
+	 * 			return true
+	 * 		end if
+	 * end while
+	 * 
+	 * return false
+	 * */
+	public boolean hasCycle(ListNode head) {
+		ListNode slow = head;
+		ListNode fast = head;
+		
+		while (fast != null && fast.next != null) {																	// T : O(n)
+			slow = slow.next;
+			fast = fast.next.next;
+			if (slow == fast) { return true; }
+		}
+		
+		return false;
+	}
+	
+}
