d8ca6609123f7dfc9fd237e39f2cfbb947147656
Sorano Lan <soranolan@example.com>
2021-10-22 13:15:56 +0800

    extract main to test

--- src/main/java/com/example/lcpjava/linkedlist/Lcp_707_Design_Linked_List.java
+++ src/main/java/com/example/lcpjava/linkedlist/Lcp_707_Design_Linked_List.java
@@ -1,34 +1,5 @@
 package com.example.lcpjava.linkedlist;
 
-import com.example.lcpjava.linkedlist.MyLinkedList.Node;
-
 public class Lcp_707_Design_Linked_List {
-	
-	public static void main(String[] args) {
-		MyLinkedList obj = new MyLinkedList();
-		
-//		obj.addAtHead(2);
-//		obj.deleteAtIndex(1);
-//		obj.addAtHead(2);
-//		obj.addAtHead(7);
-//		obj.addAtHead(3);
-//		obj.addAtHead(2);
-//		obj.addAtHead(5);
-//		obj.addAtTail(5);
-//		obj.get(5);
-//		obj.deleteAtIndex(6);
-//		obj.deleteAtIndex(4);
-		
-		obj.addAtHead(1);
-		obj.deleteAtIndex(0);
-		
-		Node temp = obj.head;
-		while (temp != null) {
-			System.out.print(temp.val + " ");
-			temp = temp.next;
-		}
-		System.out.println();
-		System.out.println(obj.get(1));
-	}
 	
 }
---
+++ src/test/java/com/example/lcpjava/linkedlist/Lcp_707_Design_Linked_ListTest.java
@@ -0,0 +1,55 @@
+package com.example.lcpjava.linkedlist;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import com.example.lcpjava.linkedlist.MyLinkedList.Node;
+
+class Lcp_707_Design_Linked_ListTest {
+	
+	@Test
+	void test_case_1() {
+		MyLinkedList obj = new MyLinkedList();
+		obj.addAtHead(2);
+		obj.deleteAtIndex(1);
+		obj.addAtHead(2);
+		obj.addAtHead(7);
+		obj.addAtHead(3);
+		obj.addAtHead(2);
+		obj.addAtHead(5);
+		obj.addAtTail(5);
+		
+		int val = obj.get(5);
+		assertThat(val).isEqualTo(2);
+		
+		obj.deleteAtIndex(6);
+		obj.deleteAtIndex(4);
+		
+		List<Integer> result = new ArrayList<>();
+		Node current = obj.head;
+		while (current != null) {
+			result.add(current.val);
+			current = current.next;
+		}
+		
+		int[] expect = new int[] { 5, 2, 3, 7, 2 };
+		int[] test = result.stream().mapToInt(i -> i).toArray();
+		
+		assertThat(test).isEqualTo(expect);
+	}
+	
+	@Test
+	void test_case_2() {
+		MyLinkedList obj = new MyLinkedList();
+		obj.addAtHead(1);
+		obj.deleteAtIndex(0);
+		
+		Node test = obj.head;
+		assertThat(test).isNull();
+	}
+	
+}
