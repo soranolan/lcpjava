7683a7eecb4d82537e61464b2d866ac33b6575af
Sorano Lan <soranolan@example.com>
2021-10-23 13:34:01 +0800

    two pointer technique

---
+++ src/main/java/com/example/lcpjava/linkedlist/Lcp_142_Linked_List_Cycle_II.java
@@ -0,0 +1,70 @@
+package com.example.lcpjava.linkedlist;
+
+public class Lcp_142_Linked_List_Cycle_II {
+	
+	class ListNode {
+		int val;
+		ListNode next;
+		ListNode(int x) {
+			val = x;
+			next = null;
+		}
+	}
+	
+	/**
+	 * time  : O(n)
+	 * space : O(1)
+	 * 
+	 * ListNode slow <- head
+	 * ListNode fast <- head
+	 * boolean isCycle <- false
+	 * 
+	 * while fast is not equal to null && fast next is not equal to null
+	 * 		slow <- slow next
+	 * 		fast <- fast next next
+	 * 		if slow is equal to fast
+	 * 			isCycle <- true
+	 * 			break
+	 * 		end if
+	 * end while
+	 * 
+	 * if isCycle is equal to false
+	 * 		return null
+	 * end if
+	 * 
+	 * slow <- head
+	 * 
+	 * while slow is not equal to fast
+	 * 		slow <- slow next
+	 * 		fast <- fast next
+	 * end while
+	 * 
+	 * return slow
+	 * */
+	public ListNode detectCycle(ListNode head) {
+		ListNode slow = head;
+		ListNode fast = head;
+		boolean isCycle = false;
+		
+		while (fast != null && fast.next != null) {																	// T : O(n)
+			slow = slow.next;
+			fast = fast.next.next;
+			if (slow == fast) {
+				isCycle = true;
+				break;
+			}
+		}
+		
+		if (isCycle == false) { return null; }
+		
+		slow = head;
+		
+		while (slow != fast) {																						// T : O(n)
+			slow = slow.next;
+			fast = fast.next;
+		}
+		
+		return slow;
+	}
+	
+}
---
+++ src/test/java/com/example/lcpjava/linkedlist/Lcp_142_Linked_List_Cycle_IITest.java
@@ -0,0 +1,56 @@
+package com.example.lcpjava.linkedlist;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import com.example.lcpjava.linkedlist.Lcp_142_Linked_List_Cycle_II.ListNode;
+
+class Lcp_142_Linked_List_Cycle_IITest {
+	
+	@Test
+	void test_case_1() {
+		Lcp_142_Linked_List_Cycle_II lcp = new Lcp_142_Linked_List_Cycle_II();
+		
+		ListNode node1 = lcp.new ListNode(3);
+		ListNode node2 = lcp.new ListNode(2);
+		ListNode node3 = lcp.new ListNode(0);
+		ListNode node4 = lcp.new ListNode(4);
+		
+		node1.next = node2;
+		node2.next = node3;
+		node3.next = node4;
+		node4.next = node2;
+		
+		ListNode test = lcp.detectCycle(node1);
+		
+		assertThat(test).isEqualTo(node2);
+	}
+	
+	@Test
+	void test_case_2() {
+		Lcp_142_Linked_List_Cycle_II lcp = new Lcp_142_Linked_List_Cycle_II();
+		
+		ListNode node1 = lcp.new ListNode(1);
+		ListNode node2 = lcp.new ListNode(2);
+		
+		node1.next = node2;
+		node2.next = node1;
+		
+		ListNode test = lcp.detectCycle(node1);
+		
+		assertThat(test).isEqualTo(node1);
+	}
+	
+	@Test
+	void test_case_3() {
+		Lcp_142_Linked_List_Cycle_II lcp = new Lcp_142_Linked_List_Cycle_II();
+		
+		ListNode node1 = lcp.new ListNode(1);
+		
+		ListNode test = lcp.detectCycle(node1);
+		
+		assertThat(test).isNull();
+	}
+	
+}
