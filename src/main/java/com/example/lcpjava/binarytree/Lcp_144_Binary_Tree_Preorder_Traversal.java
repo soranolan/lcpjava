c1db7463316fa576e3c6b90186f1f8ba24a06a35
Sorano Lan <soranolan@example.com>
2021-12-13 17:22:12 +0800

    Traverse a tree

---
+++ src/main/java/com/example/lcpjava/binarytree/Lcp_144_Binary_Tree_Preorder_Traversal.java
@@ -0,0 +1,122 @@
+package com.example.lcpjava.binarytree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Lcp_144_Binary_Tree_Preorder_Traversal {
+	
+	/**
+	 * time  : O(n)
+	 * space : O(n)
+	 * 
+	 * List<Integer> result <- new ArrayList
+	 * traversal(root, result)
+	 * return result
+	 * */
+	public List<Integer> preorderTraversal_1(TreeNode root) {
+		List<Integer> result = new ArrayList<>();
+		traversal(root, result);
+		return result;
+	}
+	
+	/**
+	 * if current is equal to null
+	 * 		return
+	 * end if
+	 * 
+	 * result add(current val)
+	 * traversal(current left, result)
+	 * traversal(current right, result)
+	 * */
+	public void traversal(TreeNode current, List<Integer> result) {
+		if (current == null) { return; }
+		result.add(current.val);
+		traversal(current.left, result);
+		traversal(current.right, result);
+	}
+	
+	/**
+	 * time  : O(n)
+	 * space : O(n)
+	 * 
+	 * List<Integer> result <- new ArrayList
+	 * if root is equal to null
+	 * 		return result
+	 * end if
+	 * 
+	 * Stack<TreeNode> stack <- new Stack
+	 * 
+	 * TreeNode current <- root
+	 * stack push(current)
+	 * 
+	 * while stack is not empty
+	 * 		current <- stack pop
+	 * 		result add(current val)
+	 * 		if current left is not equal to null
+	 * 			stack push(current right)
+	 * 		end if
+	 * 		if current right is not equal to null
+	 * 			stack push(current left)
+	 * 		end if
+	 * end while
+	 * 
+	 * return result
+	 * */
+	public List<Integer> preorderTraversal_2(TreeNode root) {
+		List<Integer> result = new ArrayList<>();
+		if (root == null) { return result; }
+		Stack<TreeNode> stack = new Stack<>();																		// S : O(n)
+		
+		TreeNode current = root;
+		stack.push(current);
+		
+		while (!stack.isEmpty()) {																					// T : O(n)
+			current = stack.pop();
+			result.add(current.val);
+			if (current.right != null) { stack.push(current.right); }
+			if (current.left != null) { stack.push(current.left); }
+		}
+		
+		return result;
+	}
+	
+	/**
+	 * time  : O(n)
+	 * space : O(node right)
+	 * 
+	 * List<Integer> result <- new ArrayList
+	 * Stack<TreeNode> stack <- new Stack
+	 * TreeNode current <- root
+	 * 
+	 * while current is not equal to null || stack is not empty
+	 * 		if current is not equal to null
+	 * 			stack push(current right)
+	 * 			result add(current val)
+	 * 			current <- current left
+	 * 		else
+	 * 			current <- stack pop
+	 * 		end if
+	 * end while
+	 * 
+	 * return result
+	 * */
+	public List<Integer> preorderTraversal(TreeNode root) {
+		List<Integer> result = new ArrayList<>();
+		Stack<TreeNode> stack = new Stack<>();																		// S : O(node right)
+		TreeNode current = root;
+		
+		while (current != null || !stack.isEmpty()) {																// T : O(n)
+			if (current != null) {
+				stack.push(current.right);
+				result.add(current.val);
+				current = current.left;
+			} else {
+				current = stack.pop();
+			}
+		}
+		
+		return result;
+	}
+	
+}
---
+++ src/test/java/com/example/lcpjava/binarytree/Lcp_144_Binary_Tree_Preorder_TraversalTest.java
@@ -0,0 +1,87 @@
+package com.example.lcpjava.binarytree;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.Test;
+
+class Lcp_144_Binary_Tree_Preorder_TraversalTest {
+	
+	@Test
+	void test_case_1() {
+		Lcp_144_Binary_Tree_Preorder_Traversal lcp = new Lcp_144_Binary_Tree_Preorder_Traversal();
+		
+		List<Integer> expected = Arrays.stream(new Integer[] { 1, 2, 3 }).collect(Collectors.toList());
+		
+		TreeNode root = new TreeNode(1);
+		TreeNode r1 = new TreeNode(2);
+		TreeNode r1l1 = new TreeNode(3);
+		
+		root.right = r1;
+		r1.left = r1l1;
+		
+		List<Integer> test = lcp.preorderTraversal(root);
+		
+		assertThat(test).isEqualTo(expected);
+	}
+	
+	@Test
+	void test_case_2() {
+		Lcp_144_Binary_Tree_Preorder_Traversal lcp = new Lcp_144_Binary_Tree_Preorder_Traversal();
+		
+		List<Integer> expected = Arrays.stream(new Integer[] {  }).collect(Collectors.toList());
+		
+		List<Integer> test = lcp.preorderTraversal(null);
+		
+		assertThat(test).isEqualTo(expected);
+	}
+	
+	@Test
+	void test_case_3() {
+		Lcp_144_Binary_Tree_Preorder_Traversal lcp = new Lcp_144_Binary_Tree_Preorder_Traversal();
+		
+		List<Integer> expected = Arrays.stream(new Integer[] { 1 }).collect(Collectors.toList());
+		
+		TreeNode root = new TreeNode(1);
+		
+		List<Integer> test = lcp.preorderTraversal(root);
+		
+		assertThat(test).isEqualTo(expected);
+	}
+	
+	@Test
+	void test_case_4() {
+		Lcp_144_Binary_Tree_Preorder_Traversal lcp = new Lcp_144_Binary_Tree_Preorder_Traversal();
+		
+		List<Integer> expected = Arrays.stream(new Integer[] { 1, 2 }).collect(Collectors.toList());
+		
+		TreeNode root = new TreeNode(1);
+		TreeNode l1 = new TreeNode(2);
+		
+		root.left = l1;
+		
+		List<Integer> test = lcp.preorderTraversal(root);
+		
+		assertThat(test).isEqualTo(expected);
+	}
+	
+	@Test
+	void test_case_5() {
+		Lcp_144_Binary_Tree_Preorder_Traversal lcp = new Lcp_144_Binary_Tree_Preorder_Traversal();
+		
+		List<Integer> expected = Arrays.stream(new Integer[] { 1, 2 }).collect(Collectors.toList());
+		
+		TreeNode root = new TreeNode(1);
+		TreeNode r1 = new TreeNode(2);
+		
+		root.right = r1;
+		
+		List<Integer> test = lcp.preorderTraversal(root);
+		
+		assertThat(test).isEqualTo(expected);
+	}
+	
+}
