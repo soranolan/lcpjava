79840a785b65bf247018aef6f0a9de48332bde70
Sorano Lan <soranolan@example.com>
2021-12-18 16:19:20 +0800

    Solve Problems Recursively

---
+++ src/main/java/com/example/lcpjava/binarytree/Lcp_101_Symmetric_Tree.java
@@ -0,0 +1,100 @@
+package com.example.lcpjava.binarytree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Lcp_101_Symmetric_Tree {
+	
+	/**
+	 * time  : O(n)
+	 * space : O(n)
+	 * 
+	 * if root is equal to null
+	 * 		return true
+	 * end if
+	 * 
+	 * return isEqual(root left, root right)
+	 * */
+	public boolean isSymmetric_1(TreeNode root) {
+		if (root == null) { return true; }
+		return isEqual(root.left, root.right);
+	}
+	
+	/**
+	 * if left is equal to null || right is equal to null
+	 * 		return left is equal to null
+	 * end if
+	 * 
+	 * if left val is not equal to right val
+	 * 		return false
+	 * end if
+	 * 
+	 * return isEqual(left left, right right) && isEqual(left right, right left)
+	 * */
+	public boolean isEqual(TreeNode left, TreeNode right) {
+		if (left == null || right == null) { return left == right; }
+		if (left.val != right.val) { return false; }
+		return isEqual(left.left, right.right) && isEqual(left.right, right.left);
+	}
+	
+	/**
+	 * time  : O(n)
+	 * space : O(n)
+	 * 
+	 * if root is equal to null
+	 * 		return true
+	 * end if
+	 * 
+	 * Queue<TreeNode> queue <- new LinkedList
+	 * queue offer(root left)
+	 * queue offer(root right)
+	 * 
+	 * while queue is not empty
+	 * 		TreeNode left <- queue poll
+	 * 		TreeNode right <- queue poll
+	 * 		
+	 * 		if left is equal to null && right is equal to null
+	 * 			continue
+	 * 		end if
+	 * 		
+	 * 		if left is equal to null || right is equal to null
+	 * 			return false
+	 * 		end if
+	 * 		
+	 * 		if left val is not equal to right val
+	 * 			return false
+	 * 		end if
+	 * 		
+	 * 		queue offer(left left)
+	 * 		queue offer(right right)
+	 * 		queue offer(left right)
+	 * 		queue offer(right left)
+	 * end while
+	 * 
+	 * return true
+	 * */
+	public boolean isSymmetric(TreeNode root) {
+		if (root == null) { return true; }
+		
+		Queue<TreeNode> queue = new LinkedList<>();																	// S : O(n)
+		queue.offer(root.left);																						// T : O(1)
+		queue.offer(root.right);																					// T : O(1)
+		
+		while (!queue.isEmpty()) {																					// T : O(n)
+			TreeNode left = queue.poll();																			// T : O(1)
+			TreeNode right = queue.poll();																			// T : O(1)
+			
+			if (left == null && right == null) { continue; }
+			if (left == null || right == null) { return false; }
+			if (left.val != right.val) { return false; }
+			
+			queue.offer(left.left);																					// T : O(1)
+			queue.offer(right.right);																				// T : O(1)
+			queue.offer(left.right);																				// T : O(1)
+			queue.offer(right.left);																				// T : O(1)
+		}
+		
+		return true;
+	}
+	
+}
---
+++ src/test/java/com/example/lcpjava/binarytree/Lcp_101_Symmetric_TreeTest.java
@@ -0,0 +1,53 @@
+package com.example.lcpjava.binarytree;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class Lcp_101_Symmetric_TreeTest {
+	
+	@Test
+	void test_case_1() {
+		Lcp_101_Symmetric_Tree lcp = new Lcp_101_Symmetric_Tree();
+		
+		TreeNode root = new TreeNode(1);
+		TreeNode l1 = new TreeNode(2);
+		TreeNode l1l1 = new TreeNode(3);
+		TreeNode l1r1 = new TreeNode(4);
+		TreeNode r1 = new TreeNode(2);
+		TreeNode r1l1 = new TreeNode(4);
+		TreeNode r1r1 = new TreeNode(3);
+		
+		root.left = l1;
+		root.right = r1;
+		l1.left = l1l1;
+		l1.right = l1r1;
+		r1.left = r1l1;
+		r1.right = r1r1;
+		
+		boolean test = lcp.isSymmetric(root);
+		
+		assertThat(test).isTrue();
+	}
+	
+	@Test
+	void test_case_2() {
+		Lcp_101_Symmetric_Tree lcp = new Lcp_101_Symmetric_Tree();
+		
+		TreeNode root = new TreeNode(1);
+		TreeNode l1 = new TreeNode(2);
+		TreeNode l1r1 = new TreeNode(3);
+		TreeNode r1 = new TreeNode(2);
+		TreeNode r1r1 = new TreeNode(3);
+		
+		root.left = l1;
+		root.right = r1;
+		l1.right = l1r1;
+		r1.right = r1r1;
+		
+		boolean test = lcp.isSymmetric(root);
+		
+		assertThat(test).isFalse();
+	}
+	
+}
