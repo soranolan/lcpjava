013fefb9a574f3e08521c1493a9a4fd46269b669
Sorano Lan <soranolan@example.com>
2021-12-26 18:22:34 +0800

    add solution

--- src/main/java/com/example/lcpjava/binarytree/Lcp_116_Populating_Next_Right_Pointers_in_Each_Node.java
+++ src/main/java/com/example/lcpjava/binarytree/Lcp_116_Populating_Next_Right_Pointers_in_Each_Node.java
@@ -34,14 +34,19 @@ public class Lcp_116_Populating_Next_Right_Pointers_in_Each_Node {
 	 * 			
 	 * 			previous <- current
 	 * 			
-	 * 			queue offer(current left)
-	 * 			queue offer(current right)
+	 * 			if current left is not equal to null
+	 * 				queue offer(current left)
+	 * 			end if
+	 * 			
+	 * 			if current right is not equal to null
+	 * 				queue offer(current right)
+	 * 			end if
 	 * 		end while
 	 * end while
 	 * 
 	 * return root
 	 * */
-	public Node connect(Node root) {
+	public Node connect_1(Node root) {
 		if (root == null) { return null; }
 		
 		Queue<Node> queue = new LinkedList<>();																		// S : O(n)
@@ -59,12 +64,84 @@ public class Lcp_116_Populating_Next_Right_Pointers_in_Each_Node {
 				if (previous != null) { previous.next = current; }
 				previous = current;
 				
-				queue.offer(current.left);																			// T : O(1)
-				queue.offer(current.right);																			// T : O(1)
+				if (current.left != null) { queue.offer(current.left); }											// T : O(1)
+				if (current.right != null) { queue.offer(current.right); }											// T : O(1)
 			}
 		}
 		
 		return root;
+	}
+	
+	/**
+	 * time  : O(n)
+	 * space : O(n)
+	 * 
+	 * if root is equal to null
+	 * 		return null
+	 * end if
+	 * 
+	 * DFS(root left, root right)
+	 * 
+	 * return root
+	 * */
+	public Node connect_2(Node root) {
+		if (root == null) { return null; }
+		DFS_1(root.left, root.right);
+		return root;
+	}
+	
+	/**
+	 * if left is equal to null || right is equal to null
+	 * 		return
+	 * end if
+	 * 
+	 * if left next is not equal to null
+	 * 		return
+	 * end if
+	 * 
+	 * left next <- right
+	 * 
+	 * DFS(left left, left right)
+	 * DFS(left right, right left)
+	 * DFS(right left, right right)
+	 * */
+	public void DFS_1(Node left, Node right) {
+		if (left == null || right == null) { return; }
+		if (left.next != null) { return; }
+		left.next = right;
+		DFS_1(left.left, left.right);
+		DFS_1(left.right, right.left);
+		DFS_1(right.left, right.right);
+	}
+	
+	/**
+	 * time  : O(n)
+	 * space : O(n)
+	 * 
+	 * DFS(root, null)
+	 * 
+	 * return root
+	 * */
+	public Node connect(Node root) {
+		DFS(root, null);
+		return root;
+	}
+	
+	/**
+	 * if current is equal to null
+	 * 		return
+	 * end if
+	 * 
+	 * current next <- nextNode
+	 * 
+	 * DFS(current left, current right)
+	 * DFS(current right, if current next is equal to null then null else current next left)
+	 * */
+	public void DFS(Node current, Node nextNode) {
+		if (current == null) { return; }
+		current.next = nextNode;
+		DFS(current.left, current.right);
+		DFS(current.right, current.next == null ? null : current.next.left);
 	}
 	
 }
