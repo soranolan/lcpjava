59a87511b01172ad543cc3de7cefdfd720ec18d5
Sorano Lan <soranolan@example.com>
2022-01-06 16:00:51 +0800

    extract inner class

---
+++ src/main/java/com/example/lcpjava/hashtable/Helper.java
@@ -0,0 +1,67 @@
+package com.example.lcpjava.hashtable;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Helper {
+	
+	public static List<Node> treeToList(Node current) {
+		List<Node> list = new LinkedList<>();
+		Queue<Node> queue = new LinkedList<>();
+		queue.add(current);
+		
+		while (!queue.isEmpty()) {
+			Node node = queue.poll();
+			list.add(node);
+			if (node.right != null) { queue.add(node.right); }
+			if (node.left != null) { queue.add(node.left); }
+		}
+		
+		return list;
+	}
+	
+	public static void addNode(Node oldNode, Node newNode) {
+		if (newNode.val > oldNode.val) {
+			if (oldNode.right == null) {
+				oldNode.right = newNode;
+				return;
+			}
+			addNode(oldNode.right, newNode);
+		} else if (newNode.val < oldNode.val) {
+			if (oldNode.left == null) {
+				oldNode.left = newNode;
+				return;
+			}
+			addNode(oldNode.left, newNode);
+		}
+	}
+	
+	public static boolean containNode(Node current, int target) {
+		if (target == current.val) { return true; }
+		if (target > current.val && current.right != null) { return containNode(current.right, target); }
+		if (target < current.val && current.left != null) { return containNode(current.left, target); }
+		return false;
+	}
+	
+	public static Node removeNode(Node current, int target) {
+		if (target == current.val) {
+			if (current.right == null && current.left == null) { return null; }
+			if (current.right == null || current.left == null) { return (current.left != null) ? current.left : current.right; }
+			
+			current.val = findMax(current.left).val;
+			removeNode(current.left, current.val);
+		}
+		
+		if (target > current.val && current.right != null) { current.right = removeNode(current.right, target); }
+		if (target < current.val && current.left != null) { current.left = removeNode(current.left, target); }
+		return current;
+	}
+	
+	public static Node findMax(Node current) {
+		Node temp = current;
+		while (temp.right != null) { temp = temp.right; }
+		return temp;
+	}
+	
+}
---
+++ src/main/java/com/example/lcpjava/hashtable/Node.java
@@ -0,0 +1,17 @@
+package com.example.lcpjava.hashtable;
+
+public class Node {
+	
+	public int val;
+	
+	public Node left;
+	
+	public Node right;
+	
+	public Node() {  }
+	
+	public Node(int val) {
+		this.val = val;
+	}
+	
+}
