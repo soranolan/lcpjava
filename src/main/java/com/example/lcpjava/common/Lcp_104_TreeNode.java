4b7f6b4a32e93d4ec9a755422d15a03b27e0b224
soranolan <soranolan@example.com>
2022-04-12 16:08:55 +0800

    refactor

---
+++ src/main/java/com/example/lcpjava/common/Lcp_104_TreeNode.java
@@ -0,0 +1,23 @@
+package com.example.lcpjava.common;
+
+public class Lcp_104_TreeNode {
+	
+	public int val;
+	
+	public Lcp_104_TreeNode left;
+	
+	public Lcp_104_TreeNode right;
+	
+	public Lcp_104_TreeNode() {  }
+	
+	public Lcp_104_TreeNode(int val) {
+		this.val = val;
+	}
+	
+	public Lcp_104_TreeNode(int val, Lcp_104_TreeNode left, Lcp_104_TreeNode right) {
+		this.val = val;
+		this.left = left;
+		this.right = right;
+	}
+	
+}
--- src/test/java/com/example/lcpjava/binarytree/Lcp_104_Maximum_Depth_of_Binary_TreeTest.java
+++ src/test/java/com/example/lcpjava/binarytree/Lcp_104_Maximum_Depth_of_Binary_TreeTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 
+import com.example.lcpjava.common.Lcp_104_TreeNode;
+
 class Lcp_104_Maximum_Depth_of_Binary_TreeTest {
 	
 	@Test
@@ -12,11 +14,11 @@ class Lcp_104_Maximum_Depth_of_Binary_TreeTest {
 		
 		int expected = 3;
 		
-		TreeNode root = new TreeNode(3);
-		TreeNode l1 = new TreeNode(9);
-		TreeNode r1 = new TreeNode(20);
-		TreeNode r1l1 = new TreeNode(15);
-		TreeNode r1r1 = new TreeNode(7);
+		Lcp_104_TreeNode root = new Lcp_104_TreeNode(3);
+		Lcp_104_TreeNode l1 = new Lcp_104_TreeNode(9);
+		Lcp_104_TreeNode r1 = new Lcp_104_TreeNode(20);
+		Lcp_104_TreeNode r1l1 = new Lcp_104_TreeNode(15);
+		Lcp_104_TreeNode r1r1 = new Lcp_104_TreeNode(7);
 		
 		root.left = l1;
 		root.right = r1;
@@ -34,8 +36,8 @@ class Lcp_104_Maximum_Depth_of_Binary_TreeTest {
 		
 		int expected = 2;
 		
-		TreeNode root = new TreeNode(1);
-		TreeNode r1 = new TreeNode(2);
+		Lcp_104_TreeNode root = new Lcp_104_TreeNode(1);
+		Lcp_104_TreeNode r1 = new Lcp_104_TreeNode(2);
 		
 		root.right = r1;
 		
@@ -61,7 +63,7 @@ class Lcp_104_Maximum_Depth_of_Binary_TreeTest {
 		
 		int expected = 1;
 		
-		TreeNode root = new TreeNode(0);
+		Lcp_104_TreeNode root = new Lcp_104_TreeNode(0);
 		
 		int test = lcp.maxDepth(root);
 		
