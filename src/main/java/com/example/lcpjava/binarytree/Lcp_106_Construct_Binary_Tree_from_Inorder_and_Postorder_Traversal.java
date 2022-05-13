fef075a9ea9c779b8b1067266823c41be61ef324
soranolan <soranolan@example.com>
2022-05-13 19:52:28 +0800

    topic : daily challenge & Hash Table. add solution, add reference, refactor.

--- src/main/java/com/example/lcpjava/binarytree/Lcp_106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.java
+++ src/main/java/com/example/lcpjava/binarytree/Lcp_106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.java
@@ -12,6 +12,9 @@ public class Lcp_106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal 
 	 * time  : O(n)
 	 * space : O(n)
 	 * 
+	 * https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/discuss/34807/Java-iterative-solution-with-explanation
+	 * 
+	 * 
 	 * if inorder is equal to null || postorder is equal to null || inorder length is not equal to postorder length
 	 * 		return null
 	 * end if
@@ -59,7 +62,7 @@ public class Lcp_106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal 
 		Stack<TreeNode> stack = new Stack<>();																		// S : O(n)
 		stack.push(root);
 		
-		while (postIdx >= 0) {
+		while (postIdx >= 0) {																						// T : O(n)
 			TreeNode current = stack.peek();
 			if (current.val != inorder[inIdx]) {
 				TreeNode right = new TreeNode(postorder[postIdx]);
@@ -86,6 +89,9 @@ public class Lcp_106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal 
 	 * time  : O(n)
 	 * space : O(n)
 	 * 
+	 * https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/discuss/34782/My-recursive-Java-code-with-O(n)-time-and-O(n)-space
+	 * 
+	 * 
 	 * if inorder is equal to null || postorder is equal to null || inorder length is not equal to postorder length
 	 * 		return null
 	 * end if
@@ -101,13 +107,13 @@ public class Lcp_106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal 
 		if (inorder == null || postorder == null || inorder.length != postorder.length) { return null; }
 		
 		Map<Integer, Integer> memo = new HashMap<Integer, Integer>();												// S : O(n)
-		for (int i = 0; i < inorder.length; ++i) { memo.put(inorder[i], i); }
+		for (int i = 0; i < inorder.length; i++) { memo.put(inorder[i], i); }										// T : O(n)
 		
 		return build_2(0, inorder.length - 1, postorder, 0, postorder.length - 1, memo);
 	}
 	
 	/**
-	 * if isStart is greater than inEnd || postStart is greater than postEnd
+	 * if inStart is greater than inEnd || postStart is greater than postEnd
 	 * 		return null
 	 * end if
 	 * 
@@ -120,7 +126,7 @@ public class Lcp_106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal 
 	 * 
 	 * return current
 	 * */
-	public TreeNode build_2(int inStart, int inEnd, int[] postorder, int postStart, int postEnd, Map<Integer, Integer> memo) {
+	private TreeNode build_2(int inStart, int inEnd, int[] postorder, int postStart, int postEnd, Map<Integer, Integer> memo) {
 		if (inStart > inEnd || postStart > postEnd) { return null; }
 		
 		TreeNode current = new TreeNode(postorder[postEnd]);
@@ -137,17 +143,82 @@ public class Lcp_106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal 
 	 * time  : O(n)
 	 * space : O(n)
 	 * 
+	 * https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/discuss/34782/My-recursive-Java-code-with-O(n)-time-and-O(n)-space
+	 * 
+	 * 
+	 * if inorder is equal to null || postorder is equal to null || inorder length is not equal to postorder length
+	 * 		return null
+	 * end if
+	 * 
+	 * int length <- postorder length
+	 * postIndex <- length minus one
+	 * 
+	 * Map<Integer, Integer> memo <- new HashMap
+	 * for int i <- 0; if i is lower than inorder length; i++
+	 * 		memo put(inorder[i], i)
+	 * end for
+	 * 
+	 * return build(postorder, zero, length minus one, memo)
+	 * */
+	public TreeNode buildTree_3(int[] inorder, int[] postorder) {
+		if (inorder == null || postorder == null || inorder.length != postorder.length) { return null; }
+		
+		int length = postorder.length;
+		postIndex = length - 1;
+		
+		Map<Integer, Integer> memo = new HashMap<>();
+		for (int i = 0; i < inorder.length; i++) { memo.put(inorder[i], i); }
+		
+		return build_3(postorder, 0, length - 1, memo);
+	}
+	
+	/**
+	 * if inStart is greater than inEnd
+	 * 		return null
+	 * end if
+	 * 
+	 * TreeNode current <- new TreeNode(postorder[postIndex])
+	 * 
+	 * postIndex--
+	 * 
+	 * int rootIndex <- memo get(current val)
+	 * 
+	 * current left <- build(postorder, rootIndex plus one, inEnd, memo)
+	 * current right <- build(postorder, inStart, rootIndex minus one, memo)
+	 * 
+	 * return current
+	 * */
+	private TreeNode build_3(int[] postorder, int inStart, int inEnd, Map<Integer, Integer> memo) {
+		if (inStart > inEnd) { return null; }
+		
+		TreeNode current = new TreeNode(postorder[postIndex--]);
+		
+		int rootIndex = memo.get(current.val);
+		
+		current.right = build_3(postorder, rootIndex + 1, inEnd, memo);
+		current.left = build_3(postorder, inStart, rootIndex - 1, memo);
+		
+		return current;
+	}
+	
+	/**
+	 * time  : O(n)
+	 * space : O(n)
+	 * 
+	 * Accepted Solutions Runtime Distribution ?
+	 * 
+	 * 
 	 * inIndex <- inorder length minus one
 	 * postIndex <- postorder length minus one
 	 * 
 	 * return build(inorder, postorder, -3001)
 	 * */
-	int inIndex;
-	int postIndex;
-	public TreeNode buildTree(int[] inorder, int[] postorder) {
+	private int inIndex;
+	private int postIndex;
+	public TreeNode buildTree_4(int[] inorder, int[] postorder) {
 		inIndex = inorder.length - 1;
 		postIndex = postorder.length - 1;
-		return build(inorder, postorder, -3001);
+		return build_4(inorder, postorder, -3001);
 	}
 	
 	/**
@@ -168,7 +239,7 @@ public class Lcp_106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal 
 	 * 
 	 * return current
 	 * */
-	public TreeNode build(int[] inorder, int[] postorder, int rootValue) {
+	private TreeNode build_4(int[] inorder, int[] postorder, int rootValue) {
 		if (postIndex < 0) { return null; }
 		
 		if (inorder[inIndex] == rootValue) {
@@ -179,10 +250,84 @@ public class Lcp_106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal 
 		TreeNode current = new TreeNode(postorder[postIndex]);
 		postIndex--;
 		
-		current.right = build(inorder, postorder, current.val);
-		current.left = build(inorder, postorder, rootValue);
+		current.right = build_4(inorder, postorder, current.val);
+		current.left = build_4(inorder, postorder, rootValue);
 		
 		return current;
+	}
+	
+	/**
+	 * time  : O(n)
+	 * space : O(n)
+	 * 
+	 * Accepted Solutions Runtime Distribution
+	 * https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/discuss/1589310/C%2B%2BPython-2-Simple-Solutions-w-Images-and-Detailed-Explanation-or-Recursion-%2B-Hashmap
+	 * 
+	 * if inorder is equal to null || postorder is equal to null || inorder length is not equal to postorder length
+	 * 		return null
+	 * end if
+	 * 
+	 * postIndex <- postorder length minus one
+	 * 
+	 * return build(inorder, postorder, zero, postIndex)
+	 * */
+	public TreeNode buildTree(int[] inorder, int[] postorder) {
+		if (inorder == null || postorder == null || inorder.length != postorder.length) { return null; }
+		
+		postIndex = postorder.length - 1;
+		
+		return build(inorder, postorder, 0, postIndex);
+	}
+	
+	/**
+	 * if inStart is greater than inEnd
+	 * 		return null
+	 * end if
+	 * 
+	 * TreeNode current <- new TreeNode(postorder[postIndex])
+	 * 
+	 * postIndex--
+	 * 
+	 * if inStart is equal to inEnd
+	 * 		return current
+	 * end if
+	 * 
+	 * int rootIndex <- find(inEnd, current val, inorder)
+	 * 
+	 * current right <- build(inorder, postorder, rootIndex plus one, inEnd)
+	 * current left <- build(inorder, postorder, inStart, rootIndex minus one)
+	 * 
+	 * return current
+	 * */
+	private TreeNode build(int[] inorder, int[] postorder, int inStart, int inEnd) {
+		if (inStart > inEnd) { return null; }
+		
+		TreeNode current = new TreeNode(postorder[postIndex--]);
+		
+		if (inStart == inEnd) { return current; }
+		
+		int rootIndex = find(inEnd, current.val, inorder);
+		
+		current.right = build(inorder, postorder, rootIndex + 1, inEnd);											// why ? postorder : left -> right -> root, and we traverse from end to start : root -> right -> left
+		current.left = build(inorder, postorder, inStart, rootIndex - 1);
+		
+		return current;
+	}
+	
+	/**
+	 * for int i <- inEnd; if i is greater than or equal to zero; i--
+	 * 		if inorder[i] is equal to target
+	 * 			return i
+	 * 		end if
+	 * end for
+	 * 
+	 * return zero
+	 * */
+	private int find(int inEnd, int target, int[] inorder) {
+		for (int i = inEnd; i >= 0; i--) {
+			if (inorder[i] == target) { return i; }
+		}
+		return 0;
 	}
 	
 }
--- src/main/java/com/example/lcpjava/binarytree/Lcp_117_Populating_Next_Right_Pointers_in_Each_Node_II.java
+++ src/main/java/com/example/lcpjava/binarytree/Lcp_117_Populating_Next_Right_Pointers_in_Each_Node_II.java
@@ -74,10 +74,73 @@ public class Lcp_117_Populating_Next_Right_Pointers_in_Each_Node_II {
 	 * time  : O(n)
 	 * space : O(n)
 	 * 
-	 * DFS(root)
+	 * if root is equal to null
+	 * 		return null
+	 * end if
+	 * 
+	 * Queue<Node> queue <- new LinkedList
+	 * queue offer(root)
+	 * 
+	 * while queue is not empty
+	 * 		Node previous <- null
+	 * 		int size <- queue size
+	 * 		
+	 * 		for int i <- 0; if i is lower than size; i++
+	 * 			Node current <- queue poll
+	 * 			
+	 * 			if previous is not equal to null
+	 * 				previous next <- current
+	 * 			end if
+	 * 			
+	 * 			previous <- current
+	 * 			
+	 * 			if current left is not equal to null
+	 * 				queue offer(current left)
+	 * 			end if
+	 * 			
+	 * 			if current right is not equal to null
+	 * 				queue offer(current right)
+	 * 			end if
+	 * 		end for
+	 * end while
+	 * 
 	 * return root
 	 * */
 	public Lcp_117_Node connect_2(Lcp_117_Node root) {
+		if (root == null) { return null; }
+		
+		Queue<Lcp_117_Node> queue = new LinkedList<>();																// S : O(n)
+		queue.offer(root);																							// T : O(1)
+		
+		while (!queue.isEmpty()) {																					// T : O(n)
+			Lcp_117_Node previous = null;
+			int size = queue.size();																				// T : O(1)
+			
+			for (int i = 0; i < size; i++) {
+				Lcp_117_Node current = queue.poll();																// T : O(1)
+				
+				if (previous != null) { previous.next = current; }
+				previous = current;
+				
+				if (current.left != null) { queue.offer(current.left); }											// T : O(1)
+				if (current.right != null) { queue.offer(current.right); }											// T : O(1)
+			}
+		}
+		
+		return root;
+	}
+	
+	/**
+	 * time  : O(n)
+	 * space : O(n)
+	 * 
+	 * https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/discuss/472675/Short-Java-solution-with-explanation-100-runtime-and-100-memory
+	 * 
+	 * 
+	 * DFS(root)
+	 * return root
+	 * */
+	public Lcp_117_Node connect_3(Lcp_117_Node root) {
 		DFS(root);
 		return root;
 	}
@@ -103,7 +166,7 @@ public class Lcp_117_Populating_Next_Right_Pointers_in_Each_Node_II {
 	 * 		DFS(current left)
 	 * end if
 	 * */
-	public void DFS(Lcp_117_Node current) {
+	private void DFS(Lcp_117_Node current) {
 		if (current == null) { return; }
 		if (current.left != null) { current.left.next = (current.right != null) ? current.right : findNext(current); }
 		if (current.right != null) { current.right.next = findNext(current); }
@@ -112,28 +175,28 @@ public class Lcp_117_Populating_Next_Right_Pointers_in_Each_Node_II {
 	}
 	
 	/**
-	 * Node temp <- current
+	 * Node parent <- current
 	 * 
-	 * while temp next is not equal to null
-	 * 		temp <- temp next
+	 * while parent next is not equal to null
+	 * 		parent <- parent next
 	 * 		
-	 * 		if temp left is not equal to null
-	 * 			return temp left
+	 * 		if parent left is not equal to null
+	 * 			return parent left
 	 * 		end if
 	 * 		
-	 * 		if temp right is not equal to null
-	 * 			return temp right
+	 * 		if parent right is not equal to null
+	 * 			return parent right
 	 * 		end if
 	 * end while
 	 * 
 	 * return null
 	 * */
-	public Lcp_117_Node findNext(Lcp_117_Node current) {
-		Lcp_117_Node temp = current;
-		while (temp.next != null) {
-			temp = temp.next;
-			if (temp.left != null) { return temp.left; }
-			if (temp.right != null) { return temp.right; }
+	private Lcp_117_Node findNext(Lcp_117_Node current) {
+		Lcp_117_Node parent = current;
+		while (parent.next != null) {
+			parent = parent.next;
+			if (parent.left != null) { return parent.left; }
+			if (parent.right != null) { return parent.right; }
 		}
 		return null;
 	}
@@ -142,25 +205,28 @@ public class Lcp_117_Populating_Next_Right_Pointers_in_Each_Node_II {
 	 * time  : O(n)
 	 * space : O(1)
 	 * 
+	 * https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/discuss/37813/Java-solution-with-constant-space
+	 * 
+	 * 
 	 * Node head <- new Node(0)
 	 * Node tail <- head
-	 * Node current <- root
+	 * Node parent <- root
 	 * 
-	 * while current is not equal to null
-	 * 		if current left is not equal to null
-	 * 			tail next <- current left
+	 * while parent is not equal to null
+	 * 		if parent left is not equal to null
+	 * 			tail next <- parent left
 	 * 			tail <- tail next
 	 * 		end if
 	 * 		
-	 * 		if current right is not equal to null
-	 * 			tail next <- current right
+	 * 		if parent right is not equal to null
+	 * 			tail next <- parent right
 	 * 			tail <- tail next
 	 * 		end if
 	 * 		
-	 * 		current <- current next
+	 * 		parent <- parent next
 	 * 		
-	 * 		if current is equal to null
-	 * 			current <- head next
+	 * 		if parent is equal to null
+	 * 			parent <- head next
 	 * 			head next <- null
 	 * 			tail <- head
 	 * 		end if
@@ -171,22 +237,22 @@ public class Lcp_117_Populating_Next_Right_Pointers_in_Each_Node_II {
 	public Lcp_117_Node connect(Lcp_117_Node root) {
 		Lcp_117_Node head = new Lcp_117_Node(0);
 		Lcp_117_Node tail = head;
-		Lcp_117_Node current = root;
+		Lcp_117_Node parent = root;
 		
-		while (current != null) {
-			if (current.left != null) {
-				tail.next = current.left;
+		while (parent != null) {
+			if (parent.left != null) {
+				tail.next = parent.left;
 				tail = tail.next;
 			}
 			
-			if (current.right != null) {
-				tail.next = current.right;
+			if (parent.right != null) {
+				tail.next = parent.right;
 				tail = tail.next;
 			}
 			
-			current = current.next;
-			if (current == null) {
-				current = head.next;																				// root <- root left node
+			parent = parent.next;
+			if (parent == null) {
+				parent = head.next;																					// root <- root left node
 				head.next = null;
 				tail = head;
 			}
