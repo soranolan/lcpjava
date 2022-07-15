5a0e093f042501c79638b4f40a7c1da6eb21e343
soranolan <soranolan@example.com>
2022-07-15 14:28:38 +0800

    change class access modifier

--- src/test/java/com/example/lcpjava/array/Lcp_5_Longest_Palindromic_SubstringTest.java
+++ src/test/java/com/example/lcpjava/array/Lcp_5_Longest_Palindromic_SubstringTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class Lcp_5_Longest_Palindromic_SubstringTest {
+class Lcp_5_Longest_Palindromic_SubstringTest {
 	
 	private Lcp_5_Longest_Palindromic_Substring lcp;
 	
