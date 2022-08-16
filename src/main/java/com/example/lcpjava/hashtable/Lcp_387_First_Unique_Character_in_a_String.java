ed66b7f75223585e84ebffcbe5ab20e14dac99b2
soranolan <soranolan@example.com>
2022-08-16 13:14:45 +0800

    topic : daily challenge

--- src/main/java/com/example/lcpjava/hashtable/Lcp_387_First_Unique_Character_in_a_String.java
+++ src/main/java/com/example/lcpjava/hashtable/Lcp_387_First_Unique_Character_in_a_String.java
@@ -9,40 +9,6 @@ public class Lcp_387_First_Unique_Character_in_a_String {
 	/**
 	 * time  : O(n)
 	 * space : O(n)
-	 * 
-	 * int minIndex <- 100000
-	 * Map<Character, Integer> countMemo <- new HashMap
-	 * Map<Character, Integer> indexMemo <- new HashMap
-	 * 
-	 * for int i <- 0; if i is lower than s length; i++
-	 * 		char c <- s charAt(i)
-	 * 		
-	 * 		if countMemo does not containsKey(c)
-	 * 			countMemo put(c, 1)
-	 * 		else
-	 * 			countMemo put(c, countMemo get(c) plus one)
-	 * 		end if
-	 * 		
-	 * 		if indexMemo does not containsKey(c)
-	 * 			indexMemo put(c, i)
-	 * 		end if
-	 * end for
-	 * 
-	 * for int i <- 0; if i is lower than s length; i++
-	 * 		char c <- s charAt(i)
-	 * 		
-	 * 		int count <- countMemo get(c)
-	 * 		if count is greater than one
-	 * 			continue
-	 * 		end if
-	 * 		
-	 * 		int index <- indexMemo get(c)
-	 * 		if index is lower than minIndex
-	 * 			minIndex <- index
-	 * 		end if
-	 * end for
-	 * 
-	 * return if minIndex is equal to 100000 then minus one else minIndex
 	 * */
 	public int firstUniqChar_1(String s) {
 		int minIndex = 100000;
@@ -74,34 +40,6 @@ public class Lcp_387_First_Unique_Character_in_a_String {
 	/**
 	 * time  : O(n)
 	 * space : O(n)
-	 * 
-	 * int minIndex <- 100000
-	 * Map<Character, Integer> memo <- new HashMap
-	 * 
-	 * for int i <- 0; if i is lower than s length; i++
-	 * 		char c <- s charAt(i)
-	 * 		
-	 * 		if memo containsKey(c)
-	 * 			memo put(c, minus one)
-	 * 		else
-	 * 			memo put(c, i)
-	 * 		end if
-	 * end for
-	 * 
-	 * for int i <- 0; if i is lower than s length; i++
-	 * 		char c <- s charAt(i)
-	 * 		int index <- memo get(c)
-	 * 		
-	 * 		if index is equal to minus one
-	 * 			continue
-	 * 		end if
-	 * 		
-	 * 		if index is lower than minIndex
-	 * 			minIndex <- index
-	 * 		end if
-	 * end for
-	 * 
-	 * return if minIndex is equal to 100000 then minus one else minIndex
 	 * */
 	public int firstUniqChar_2(String s) {
 		int minIndex = 100000;
@@ -130,29 +68,6 @@ public class Lcp_387_First_Unique_Character_in_a_String {
 	/**
 	 * time  : O(n)
 	 * space : O(n)
-	 * 
-	 * Map<Character, Integer> memo <- new HashMap
-	 * 
-	 * for int i <- 0; if i is lower than s length; i++
-	 * 		char c <- s charAt(i)
-	 * 		
-	 * 		if memo containsKey(c)
-	 * 			memo put(c, memo get(c) plus one)
-	 * 		else
-	 * 			memo put(c, one)
-	 * 		end if
-	 * end for
-	 * 
-	 * for int i <- 0; if i is lower than s length; i++
-	 * 		char c <- s charAt(i)
-	 * 		int count <- memo get(c)
-	 * 		
-	 * 		if count is equal to one
-	 * 			return i
-	 * 		end if
-	 * end for
-	 * 
-	 * return minus one
 	 * */
 	public int firstUniqChar_3(String s) {
 		Map<Character, Integer> memo = new HashMap<>();																// S : O(n)
@@ -180,25 +95,7 @@ public class Lcp_387_First_Unique_Character_in_a_String {
 	 * time  : O(n)
 	 * space : O(26)
 	 * 
-	 * int[] memo <- new int[26]
-	 * 
-	 * for int i <- 0; if i is lower than s length; i++
-	 * 		char c <- s charAt(i)
-	 * 		int index <- c minus 'a'
-	 * 		
-	 * 		memo[index] <- memo[index] + 1
-	 * end for
-	 * 
-	 * for int i <- 0; if i is lower than s length; i++
-	 * 		char c <- s charAt(i)
-	 * 		int index <- c minus 'a'
-	 * 		
-	 * 		if memo[index] is equal to one
-	 * 			return i
-	 * 		end if
-	 * end for
-	 * 
-	 * return minus one
+	 * https://leetcode.com/problems/first-unique-character-in-a-string/discuss/86348/Java-7-lines-solution-29ms
 	 * */
 	public int firstUniqChar_4(String s) {
 		int[] memo = new int[26];																					// S : O(26)
@@ -225,33 +122,6 @@ public class Lcp_387_First_Unique_Character_in_a_String {
 	 * 097 098 099 100 101 102 103 104 105 106 107 108 109 110 111 112 113 114 115 116 117 118 119 120 121 122
 	 *   a   b   c   d   e   f   g   h   i   j   k   l   m   n   o   p   q   r   s   t   u   v   w   x   y   z
 	 *  -2  -2  -2  -2  -2  -2  -2  -2  -2  -2  -2   0  -2  -2  -2  -2  -2  -2  -2  -2  -2  -2  -2  -2  -2  -2
-	 * 
-	 * int minIndex <- 100000
-	 * int[] memo <- new int[26]
-	 * Arrays fill(memo, minus two)
-	 * 
-	 * for int i <- 0; if i is lower than s length; i++
-	 * 		char c <- s charAt(i)
-	 * 		int index <- c minus 'a'
-	 * 		
-	 * 		if memo[index] is equal to minus two
-	 * 			memo[index] <- i
-	 * 		else if memo[index] is greater than or equal to zero
-	 * 			memo[index] <- minus one
-	 * 		end if
-	 * end for
-	 * 
-	 * for int index in memo
-	 * 		if index is lower than zero
-	 * 			continue
-	 * 		end if
-	 * 		
-	 * 		if index is lower than minIndex
-	 * 			minIndex <- index
-	 * 		end if
-	 * end for
-	 * 
-	 * return if minIndex is equal to 100000 then minus one else minIndex
 	 * */
 	public int firstUniqChar(String s) {
 		int minIndex = 100000;
@@ -276,5 +146,9 @@ public class Lcp_387_First_Unique_Character_in_a_String {
 		
 		return (minIndex == 100000) ? -1 : minIndex;
 	}
+	
+	/**
+	 * https://leetcode.com/problems/first-unique-character-in-a-string/discuss/86511/Java-One-Pass-Solution-with-LinkedHashMap
+	 * */
 	
 }
--- src/test/java/com/example/lcpjava/hashtable/Lcp_387_First_Unique_Character_in_a_StringTest.java
+++ src/test/java/com/example/lcpjava/hashtable/Lcp_387_First_Unique_Character_in_a_StringTest.java
@@ -2,14 +2,20 @@ package com.example.lcpjava.hashtable;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class Lcp_387_First_Unique_Character_in_a_StringTest {
 	
+	private Lcp_387_First_Unique_Character_in_a_String lcp;
+	
+	@BeforeEach
+	void beforeEach() {
+		lcp = new Lcp_387_First_Unique_Character_in_a_String();
+	}
+	
 	@Test
 	void test_case_1() {
-		Lcp_387_First_Unique_Character_in_a_String lcp = new Lcp_387_First_Unique_Character_in_a_String();
-		
 		int expected = 0;
 		String input = "leetcode";
 		int test = lcp.firstUniqChar(input);
@@ -19,8 +25,6 @@ class Lcp_387_First_Unique_Character_in_a_StringTest {
 	
 	@Test
 	void test_case_2() {
-		Lcp_387_First_Unique_Character_in_a_String lcp = new Lcp_387_First_Unique_Character_in_a_String();
-		
 		int expected = 2;
 		String input = "loveleetcode";
 		int test = lcp.firstUniqChar(input);
@@ -30,8 +34,6 @@ class Lcp_387_First_Unique_Character_in_a_StringTest {
 	
 	@Test
 	void test_case_3() {
-		Lcp_387_First_Unique_Character_in_a_String lcp = new Lcp_387_First_Unique_Character_in_a_String();
-		
 		int expected = -1;
 		String input = "aabb";
 		int test = lcp.firstUniqChar(input);
