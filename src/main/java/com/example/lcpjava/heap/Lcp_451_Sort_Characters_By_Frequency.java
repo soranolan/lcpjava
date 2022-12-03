804265c82fa3d445f1fe8c4f8e3ed88d506fda48
Sorano Lan <samuel820523@gmail.com>
2022-12-03 16:33:28 +0800

    topic : daily challenge

--- src/main/java/com/example/lcpjava/heap/Lcp_451_Sort_Characters_By_Frequency.java
+++ src/main/java/com/example/lcpjava/heap/Lcp_451_Sort_Characters_By_Frequency.java
@@ -1,7 +1,6 @@
 package com.example.lcpjava.heap;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -12,45 +11,23 @@ import java.util.Queue;
 public class Lcp_451_Sort_Characters_By_Frequency {
 	
 	/**
-	 * time  : O(n)
-	 * space : O(n + m)
-	 * 
-	 * Map<Character, Integer> countMap <- new HashMap
-	 * 
-	 * for char c in s to char array
-	 * 		countMap put(c, countMap getOrDefault(c, zero) plus one)
-	 * end for
-	 * 
-	 * Queue<Character> maxHeap <- new PriorityQueue<>((offer, previous) -> countMap get(previous) minus countMap get(offer))
-	 * for char c in countMap keySet
-	 * 		maxHeap offer(c)
-	 * end for
-	 * 
-	 * StringBuilder sb <- new StringBuilder
-	 * while maxHeap is not empty
-	 * 		char c <- maxHeap poll
-	 * 		int count <- countMap get(c)
-	 * 		
-	 * 		for int i <- 0; if i is lower than count; i++
-	 * 			sb append(c)
-	 * 		end for
-	 * end while
-	 * 
-	 * return sb to string
+	 * time  : O(n log n)
+	 * space : O(n)
 	 * */
 	public String frequencySort_1(String s) {
-		Map<Character, Integer> countMap = new HashMap<>();															// S : O(m)
-		for (char c : s.toCharArray()) { countMap.put(c, countMap.getOrDefault(c, 0) + 1); }						// T : O(n)
+		Map<Character, Integer> char2Frequency = new HashMap<>();													// S : O(n)
+		for (char c : s.toCharArray()) { char2Frequency.put(c, char2Frequency.getOrDefault(c, 0) + 1); }			// T : O(n)
 		
-		Queue<Character> maxHeap = new PriorityQueue<>(Comparator.comparingInt(countMap::get).reversed());			// S : O(m)
-		for (char c : countMap.keySet()) {																			// T : O(m)
-			maxHeap.offer(c);																						// T : O(m log m)
+		Queue<Character> maxHeap = new PriorityQueue<>(
+				(offer, previous) -> char2Frequency.get(previous) - char2Frequency.get(offer));						// S : O(n)
+		for (char c : char2Frequency.keySet()) {																	// T : O(n)
+			maxHeap.offer(c);																						// T : O(log n)
 		}
 		
 		StringBuilder sb = new StringBuilder();																		// S : O(n)
 		while (!maxHeap.isEmpty()) {																				// T : O(n)
-			char c = maxHeap.poll();																				// T : O(1)
-			int count = countMap.get(c);																			// T : O(1)
+			char c = maxHeap.poll();																				// T : O(log n)
+			int count = char2Frequency.get(c);																		// T : O(1)
 			
 			for (int i = 0; i < count; i++) { sb.append(c); }
 		}
@@ -60,57 +37,17 @@ public class Lcp_451_Sort_Characters_By_Frequency {
 	
 	/**
 	 * time  : O(n)
-	 * space : O(n + m)
-	 * 
-	 * Map<Character, Integer> countMap <- new HashMap
-	 * 
-	 * for char c in s to char array
-	 * 		countMap put(c, countMap getOrDefault(c, zero) plus one)
-	 * end for
-	 * 
-	 * int size <- s length plus one
-	 * List<List<Character>> buckets <- new ArrayList<List<Character>>(size)
-	 * 
-	 * for int i <- 0; if i is lower than size; i++
-	 * 		buckets add(new ArrayList<>())
-	 * end for
-	 * 
-	 * for char c in countMap keySet
-	 * 		int count <- countMap get(c)
-	 * 		buckets get(count) add(c)
-	 * end for
-	 * 
-	 * StringBuilder sb <- new StringBuilder
-	 * 
-	 * for int count <- buckets size minus one; if count is greater than or equal to zero; count--
-	 * 		List<Character> bucket <- buckets get(count)
-	 * 		
-	 * 		if bucket size is equal to zero
-	 * 			continue
-	 * 		end if
-	 * 		
-	 * 		Iterator<Character> it <- bucket iterator
-	 * 		
-	 * 		while it has next
-	 * 			char c <- it next
-	 * 			
-	 * 			for int i <- 0; if i is lower than count; i++
-	 * 				sb append(c)
-	 * 			end for
-	 * 		end while
-	 * end for
-	 * 
-	 * return sb to string
+	 * space : O(n)
 	 * */
 	public String frequencySort_2(String s) {
-		Map<Character, Integer> countMap = new HashMap<>();															// S : O(m)
+		Map<Character, Integer> countMap = new HashMap<>();															// S : O(n)
 		for (char c : s.toCharArray()) { countMap.put(c, countMap.getOrDefault(c, 0) + 1); }						// T : O(n)
 		
 		int size = s.length() + 1;
 		List<List<Character>> buckets = new ArrayList<List<Character>>(size);										// S : O(n + 1)
 		for (int i = 0; i < size; i++) { buckets.add(new ArrayList<>()); }											// T : O(n + 1)
 		
-		for (char c : countMap.keySet()) {																			// T : O(m)
+		for (char c : countMap.keySet()) {																			// T : O(n)
 			int count = countMap.get(c);																			// T : O(1)
 			buckets.get(count).add(c);																				// T : O(1)
 		}
@@ -131,74 +68,71 @@ public class Lcp_451_Sort_Characters_By_Frequency {
 	}
 	
 	/**
-	 * time  : O(n * 128)
+	 * time  : O(n)
 	 * space : O(n)
-	 * 
-	 * char[] result <- new char[s length]
-	 * int[] bucket <- new int[128]
-	 * 
-	 * for char c in s to char array
-	 * 		bucket[c] <- bucket[c] plus one
-	 * end for
-	 * 
-	 * int i <- 0
-	 * int length <- result length
-	 * 
-	 * while i is lower than length
-	 * 		int maxCount <- 0
-	 * 		char c <- ' '
-	 * 		
-	 * 		for int j <- 0; if j is lower than 128; j++
-	 * 			int count <- bucket[j]
-	 * 			
-	 * 			if count is lower than or equal to maxCount
-	 * 				continue
-	 * 			end if
-	 * 			
-	 * 			maxCount <- count
-	 * 			c <- j to char
-	 * 		end for
-	 * 		
-	 * 		if maxCount is equal to zero
-	 * 			return String value of result
-	 * 		end if
-	 * 		
-	 * 		for int count <- 0; if count is lower than maxCount; count++
-	 * 			result[i] <- c
-	 * 			i++
-	 * 		end for
-	 * 		
-	 * 		bucket[c] <- zero
-	 * end while
-	 * 
-	 * return String value of result
 	 * */
-	public String frequencySort(String s) {
-		char[] result = new char[s.length()];																		// S : O(n)
+	public String frequencySort_3(String s) {
+		int n = s.length();
+		char[] result = new char[n];																				// S : O(n)
+		
 		int[] bucket = new int[128];																				// S : O(128)
 		for (char c : s.toCharArray()) { bucket[c]++; }																// T : O(n)
 		
-		int i = 0;
-		int length = result.length;
-		while (i < length) {																						// T : O(n)
+		int index = 0;
+		while (index < n) {																							// T : O(n)
 			int maxCount = 0;
-			char c = ' ';
+			char c = ' ';																							// 32
 			
-			for (int j = 0; j < 128; j++) {																			// T : O(128)
-				int count = bucket[j];
-				if (count <= maxCount) { continue; }
-				
-				maxCount = count;
-				c = (char) j;
+			for (int i = 0; i < 128; i++) {																			// T : O(128)
+				if (maxCount < bucket[i]) {
+					maxCount = bucket[i];
+					c = (char) i;
+				}
 			}
 			
 			if (maxCount == 0) { return String.valueOf(result); }
 			
-			for (int count = 0; count < maxCount; count++) { result[i++] = c; }
+			for (int count = 0; count < maxCount; count++) { result[index++] = c; }									// T : O(m)
+			
 			bucket[c] = 0;
 		}
 		
 		return String.valueOf(result);
+	}
+	
+	/**
+	 * time  : O(n)
+	 * space : O(n)
+	 * 
+	 * Accepted Solutions Runtime Distribution
+	 * */
+	public String frequencySort(String s) {
+		int n = s.length();
+		
+		int[] bucket = new int[128];																				// S : O(128)
+		for (char c : s.toCharArray()) { bucket[c]++; }																// T : O(n)
+		
+		StringBuilder sb = new StringBuilder();																		// S : O(n)
+		
+		while (sb.length() != n) {																					// T : O(n)
+			int maxCount = 0;
+			char c = ' ';																							// 32
+			
+			for (int i = 0; i < 128; i++) {																			// T : O(128)
+				if (maxCount < bucket[i]) {
+					maxCount = bucket[i];
+					c = (char) i;
+				}
+			}
+			
+			if (maxCount == 0) { return sb.toString(); }
+			
+			sb.append(String.valueOf(c).repeat(maxCount));															// T : O(m)
+			
+			bucket[c] = 0;
+		}
+		
+		return sb.toString();
 	}
 	
 }
--- src/test/java/com/example/lcpjava/heap/Lcp_451_Sort_Characters_By_FrequencyTest.java
+++ src/test/java/com/example/lcpjava/heap/Lcp_451_Sort_Characters_By_FrequencyTest.java
@@ -2,14 +2,20 @@ package com.example.lcpjava.heap;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class Lcp_451_Sort_Characters_By_FrequencyTest {
 	
+	private Lcp_451_Sort_Characters_By_Frequency lcp;
+	
+	@BeforeEach
+	void beforeEach() {
+		lcp = new Lcp_451_Sort_Characters_By_Frequency();
+	}
+	
 	@Test
 	void test_case_1() {
-		Lcp_451_Sort_Characters_By_Frequency lcp = new Lcp_451_Sort_Characters_By_Frequency();
-		
 		String expected = "eert";																					// eetr is also valid
 		String input = "tree";
 		String test = lcp.frequencySort(input);
@@ -19,8 +25,6 @@ class Lcp_451_Sort_Characters_By_FrequencyTest {
 	
 	@Test
 	void test_case_2() {
-		Lcp_451_Sort_Characters_By_Frequency lcp = new Lcp_451_Sort_Characters_By_Frequency();
-		
 		String expected = "aaaccc";																					// cccaaa is also valid
 		String input = "cccaaa";
 		String test = lcp.frequencySort(input);
@@ -30,8 +34,6 @@ class Lcp_451_Sort_Characters_By_FrequencyTest {
 	
 	@Test
 	void test_case_3() {
-		Lcp_451_Sort_Characters_By_Frequency lcp = new Lcp_451_Sort_Characters_By_Frequency();
-		
 		String expected = "bbAa";																					// bbaA is also valid
 		String input = "Aabb";
 		String test = lcp.frequencySort(input);
