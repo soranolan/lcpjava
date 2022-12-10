e0d6cead5dd4d6c43003100b0bdfe5b6f5e96cc3
Sorano Lan <samuel820523@gmail.com>
2022-12-10 15:37:46 +0800

    topic : daily challenge

--- src/main/java/com/example/lcpjava/binarytree/Lcp_1339_Maximum_Product_of_Splitted_Binary_Tree.java
+++ src/main/java/com/example/lcpjava/binarytree/Lcp_1339_Maximum_Product_of_Splitted_Binary_Tree.java
@@ -10,7 +10,7 @@ public class Lcp_1339_Maximum_Product_of_Splitted_Binary_Tree {
 	 * 
 	 * https://leetcode.com/problems/maximum-product-of-splitted-binary-tree/discuss/496549/JavaC%2B%2BPython-Easy-and-Concise
 	 * */
-	public int maxProduct(TreeNode root) {
+	public int maxProduct_1(TreeNode root) {
 		long[] max = new long[1];
 		long totalSum = 0L;
 		
@@ -29,6 +29,41 @@ public class Lcp_1339_Maximum_Product_of_Splitted_Binary_Tree {
 		long subtreeSum = current.val + traversal(current.left, totalSum, max) + traversal(current.right, totalSum, max);
 		
 		max[0] = Math.max(max[0], (totalSum - subtreeSum) * subtreeSum);
+		
+		return subtreeSum;
+	}
+	
+	/**
+	 * time  : O(n)
+	 * space : O(height)
+	 * */
+	public int maxProduct(TreeNode root) {
+		long[] product = new long[1];
+		
+		long sum = getSum(root);
+		
+		getProduct(root, sum, product);
+		
+		return (int) (product[0] % 1000000007);
+	}
+	
+	private long getSum(TreeNode current) {
+		if (current == null) { return 0L; }
+		
+		long left = getSum(current.left);
+		long right = getSum(current.right);
+		
+		return current.val + left + right;
+	}
+	
+	private long getProduct(TreeNode current, long sum, long[] product) {
+		if (current == null) { return 0L; }
+		
+		long left = getProduct(current.left, sum, product);
+		long right = getProduct(current.right, sum, product);
+		long subtreeSum = current.val + left + right;
+		
+		product[0] = Math.max(product[0], (sum - subtreeSum) * subtreeSum);
 		
 		return subtreeSum;
 	}
