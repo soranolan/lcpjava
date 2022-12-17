5f8eac81982d9be278f2eaba1244f17406b41535
Sorano Lan <samuel820523@gmail.com>
2022-12-17 12:00:25 +0800

    topic : daily challenge

--- src/main/java/com/example/lcpjava/stack/Lcp_150_Evaluate_Reverse_Polish_Notation.java
+++ src/main/java/com/example/lcpjava/stack/Lcp_150_Evaluate_Reverse_Polish_Notation.java
@@ -7,34 +7,8 @@ public class Lcp_150_Evaluate_Reverse_Polish_Notation {
 	/**
 	 * time  : O(n)
 	 * space : O(n)
-	 * 
-	 * Stack<Integer> stack <- new Stack
-	 * 
-	 * for token in array
-	 * 		switch token
-	 * 			case "+"
-	 * 				stack push(stack pop plus stack pop)
-	 * 				break
-	 * 			case "-"
-	 * 				stack push(minus one times stack pop plus stack pop)
-	 * 				break
-	 * 			case "*"
-	 * 				stack push(stack pop times stack pop)
-	 * 				break
-	 * 			case "/"
-	 * 				int second <- stack pop
-	 * 				int first <- stack pop
-	 * 				stack push(first divided by second)
-	 * 				break
-	 * 			default
-	 * 				stack push(token to Integer)
-	 * 				break
-	 * 		end switch
-	 * end for
-	 * 
-	 * return stack peek
 	 * */
-	public int evalRPN(String[] tokens) {
+	public int evalRPN_1(String[] tokens) {
 		Stack<Integer> stack = new Stack<>();																		// S : O(n)
 		
 		for (String token : tokens) {																				// T : O(n)
@@ -60,6 +34,41 @@ public class Lcp_150_Evaluate_Reverse_Polish_Notation {
 		}
 		
 		return stack.peek();
+	}
+	
+	/**
+	 * time  : O(n)
+	 * space : O(n)
+	 * 
+	 * Accepted Solutions Runtime Distribution
+	 * */
+	public int evalRPN(String[] tokens) {
+		int n = tokens.length;
+		int[] nextPosition = new int[1];
+		return eval(tokens, n - 1, nextPosition);
+	}
+	
+	private int eval(String[] tokens, int currentPosition, int[] nextPosition) {
+		String token = tokens[currentPosition];
+		char operand = token.charAt(0);
+		
+		if (token.length() == 1 && (operand == '+' || operand == '-' || operand == '*' || operand == '/')) {
+			int right = eval(tokens, currentPosition - 1, nextPosition);
+			int left = eval(tokens, nextPosition[0], nextPosition);
+			
+			if (operand == '+') {
+				return left + right;
+			} else if (operand == '-') {
+				return left - right;
+			} else if (operand == '*') {
+				return left * right;
+			} else {
+				return left / right;
+			}
+		} else {
+			nextPosition[0] = currentPosition - 1;
+			return Integer.parseInt(token);
+		}
 	}
 	
 }
