ae697ffcc74045ea5de9b1c56101ca71affd7c5a
Sorano Lan <samuel820523@gmail.com>
2023-08-12 13:31:12 +0800

    topic : Explore / Learn / Binary Search Tree

--- src/test/java/com/example/lcpjava/binarysearchtree/Lcp_98_Validate_Binary_Search_TreeTest.java
+++ src/test/java/com/example/lcpjava/binarysearchtree/Lcp_98_Validate_Binary_Search_TreeTest.java
@@ -22,26 +22,50 @@ class Lcp_98_Validate_Binary_Search_TreeTest {
 	
 	@Test
 	void test_case_1() {
+		// Arrange
 		TreeNode root = tool.preorder(new Integer[] { 2, 1, 3 });
+		
+		// Act
 		boolean test = lcp.isValidBST(root);
 		
+		// Assert
 		assertThat(test).isTrue();
 	}
 	
 	@Test
 	void test_case_2() {
+		// Arrange
 		TreeNode root = tool.preorder(new Integer[] { 5, 1, 4, null, null, 3, 6 });
+		
+		// Act
 		boolean test = lcp.isValidBST(root);
 		
+		// Assert
 		assertThat(test).isFalse();
 	}
 	
 	@Test
 	void test_case_3() {
+		// Arrange
 		TreeNode root = tool.preorder(new Integer[] { 5, 4, 6, null, null, 3, 7 });
+		
+		// Act
 		boolean test = lcp.isValidBST(root);
 		
+		// Assert
 		assertThat(test).isFalse();
+	}
+	
+	@Test
+	void test_case_4() {
+		// Arrange
+		TreeNode root = tool.preorder(new Integer[] { 3, 1, 5, 0, 2, 4, 6 });
+		
+		// Act
+		boolean test = lcp.isValidBST(root);
+		
+		// Assert
+		assertThat(test).isTrue();
 	}
 	
 }
